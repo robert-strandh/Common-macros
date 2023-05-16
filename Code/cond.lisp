1f339f3aea1b72ace45cced4cfa7198133cf35f1
Robert Strandh <robert.strandh@gmail.com>
2023-05-16 11:32:12 +0200

    Rewrite COND using Iconoclast.

--- Code/cond.lisp
+++ Code/cond.lisp
@@ -1,21 +1,24 @@
 (cl:in-package #:common-macros)
 
-(defmacro cond (&rest clauses)
-  (labels ((aux (clauses)
-             (if (null clauses)
-                 nil
-                 (let ((clause (car clauses)))
-                   (if (not (and (proper-list-p clause)
-                                 (not (null clause))))
-                       (error 'malformed-cond-clause
-                              :clause clause)
-                       (if (null (cdr clause))
-                           `(or ,(car clause)
-                                ,(aux (cdr clauses)))
-                           `(if ,(car clause)
-                                (progn ,@(cdr clause))
-                                ,(aux (cdr clauses)))))))))
-    (unless (proper-list-p clauses)
-      (error 'malformed-cond-clauses
-             :clauses clauses))
-    (aux clauses)))
+(defmacro cond (&whole form &rest clauses)
+  (declare (ignore clauses))
+  (let* ((builder (make-instance 'bld:builder))
+         (syntax (ses:find-syntax 'cl:cond))
+         (form-to-parse (cons 'cl:cond (cdr form)))
+         (ast (ses:parse builder syntax form-to-parse)))
+    (labels ((process-clauses (clause-asts)
+               (if (null clause-asts)
+                   'nil
+                   (let* ((clause-ast (first clause-asts))
+                          (test-ast (ico:test-ast clause-ast))
+                          (test-form (ico:form test-ast))
+                          (form-asts (ico:form-asts clause-ast))
+                          (forms (mapcar #'ico:form form-asts)))
+                     (if (null form-asts)
+                         `(or ,test-form
+                              ,(process-clauses (rest clause-asts)))
+                         `(if ,test-form
+                              (progn ,@forms)
+                              ,(process-clauses (rest clause-asts))))))))
+      (process-clauses (ico:clause-asts ast)))))
+
