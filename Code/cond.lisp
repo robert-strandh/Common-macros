c47d13a269aca5f93abcfa03292cd6f33fd70f56
Robert Strandh <robert.strandh@gmail.com>
2023-05-16 08:55:57 +0200

    Signal errors for malformed clauses.

--- Code/cond.lisp
+++ Code/cond.lisp
@@ -7,11 +7,15 @@
                  (let ((clause (car clauses)))
                    (if (not (and (proper-list-p clause)
                                  (not (null clause))))
-                       (error "malformed clause: ~s" clause)
+                       (error 'malformed-cond-clause
+                              :clause clause)
                        (if (null (cdr clause))
                            `(or ,(car clause)
                                 ,(aux (cdr clauses)))
                            `(if ,(car clause)
                                 (progn ,@(cdr clause))
                                 ,(aux (cdr clauses)))))))))
+    (unless (proper-list-p clauses)
+      (error 'malformed-cond-clauses
+             :clauses clauses))
     (aux clauses)))
