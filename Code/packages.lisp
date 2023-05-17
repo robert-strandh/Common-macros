d8ca9cca6244c49ab53dda40c243256aaeae1038
Robert Strandh <robert.strandh@gmail.com>
2023-05-17 10:05:45 +0200

    Define separate package for the macro definitions

--- Code/packages.lisp
+++ Code/packages.lisp
@@ -1,25 +1,28 @@
 (cl:in-package #:common-lisp-user)
 
+(defpackage #:common-macro-definitions
+  (:use)
+  (:export
+   #:when
+   #:unless
+   #:and
+   #:or
+   #:cond
+   #:case
+   #:incf
+   #:decf
+   #:push
+   #:pop
+   #:return
+   #:nth-value
+   #:shiftf
+   #:rotatef
+   #:defvar
+   #:defparameter))
+
 (defpackage #:common-macros
   (:use #:common-lisp)
   (:local-nicknames (#:ico #:iconoclast)
                     (#:bld #:iconoclast-builder)
-                    (#:ses #:s-expression-syntax))
-  (:shadow
-   . #1=(#:when
-         #:unless
-         #:and
-         #:or
-         #:cond
-         #:case
-         #:incf
-         #:decf
-         #:push
-         #:pop
-         #:return
-         #:nth-value
-         #:shiftf
-         #:rotatef
-         #:defvar
-         #:defparameter))
-  (:export . #1#))
+                    (#:ses #:s-expression-syntax)
+                    (#:cmd #:common-macro-definitions)))
