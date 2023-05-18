b718631391f57d8114c66f98a528861ec0df5111
Robert Strandh <robert.strandh@gmail.com>
2023-05-18 15:01:04 +0200

    Define function SEPARATE-ORDINARY-BODY.

--- Code/utilities.lisp
+++ Code/utilities.lisp
@@ -1,1 +1,19 @@
 (cl:in-package #:common-macros)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Separate an ordinary body such as a let or let* body that may
+;;; contain declarations (but no documentation) into the declarations
+;;; and the executable forms.
+;;;
+;;; If there are declarations after the first executable form (which
+;;; is a syntax error), then those declarations will be considered
+;;; part of the executable forms.
+
+(defun separate-ordinary-body (body)
+  (let ((pos (position-if-not (lambda (item)
+                                (and (consp item)
+                                     (eq (car item) 'declare)))
+                              body)))
+    (if (null pos)
+        (values body '())
+        (values (subseq body 0 pos) (subseq body pos)))))
