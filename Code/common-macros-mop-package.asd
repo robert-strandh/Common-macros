55b07f87971fbead878419dd85a67882adb818bc
Robert Strandh <robert.strandh@gmail.com>
2023-06-03 14:21:46 +0200

    New system and package file for default MOP symbols.

---
+++ Code/common-macros-mop-package.asd
@@ -0,0 +1,6 @@
+(cl:in-package #:asdf-user)
+
+(defsystem #:common-macros-mop-package
+  :serial t
+  :components
+  ((:file "common-macros-mop-package")))
---
+++ Code/common-macros-mop-package.lisp
@@ -0,0 +1,6 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:common-macros-mop-package
+  (:use)
+  (:export #:ensure-class
+           #:ensure-method))
