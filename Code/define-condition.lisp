0ef31a06f277935e51f95f1bf4d120d1aab5c5b4
Robert Strandh <robert.strandh@gmail.com>
2023-06-19 10:47:14 +0200

    Fix bad indentation, and call ICO:UNPARSE-LAMBDA-LIST-AST.

--- Code/define-condition.lisp
+++ Code/define-condition.lisp
@@ -3,7 +3,7 @@
 (defvar *condition-class-metaclass-name*)
 
 (defun generate-lambda-expression (lambda-expression-ast)
-  `(lambda ,(generate-ordinary-lambda-list
+  `(lambda ,(ico:unparse-lambda-list-ast
              (ico:lambda-list-ast lambda-expression-ast))
      ;; FIXME: generate declarations.
      ,@(loop for form-ast in (ico:form-asts lambda-expression-ast)
@@ -27,16 +27,16 @@
          :name ,name
          :metaclass (class-of (class-of (find-class 'condition)))
          :direct-superclasses
-        ',(mapcar #'ico:name (ico:superclass-asts ast))
-        :direct-slots
-        (list ,@(mapcar #'canonicalize-slot-specifier-ast
-                        (ico:slot-specifier-asts ast)))
-        ,@(let ((default-initarg-asts (ico:default-initarg-asts ast)))
-            (if (null default-initarg-asts)
-                '()
-                `(:direct-default-initargs
-                  (list ,@(mapcar #'canonicalize-default-initarg-ast
-                                  default-initarg-asts))))))
+         ',(mapcar #'ico:name (ico:superclass-asts ast))
+         :direct-slots
+         (list ,@(mapcar #'canonicalize-slot-specifier-ast
+                         (ico:slot-specifier-asts ast)))
+         ,@(let ((default-initarg-asts (ico:default-initarg-asts ast)))
+             (if (null default-initarg-asts)
+                 '()
+                 `(:direct-default-initargs
+                   (list ,@(mapcar #'canonicalize-default-initarg-ast
+                                   default-initarg-asts))))))
        ,(let ((report-ast (ico:report-ast ast)))
           (if (null report-ast)
               (let ((method-variable (gensym)))
@@ -54,4 +54,4 @@
                           ,(ico:form argument-ast) ,stream-variable)
                         `(funcall ,(generate-lambda-expression
                                     (ico:argument-ast report-ast))
-                                  ,condition-variable ,stream-variable)))))))))
+                                  ,condition-variable ,stream-variable))))))))))
