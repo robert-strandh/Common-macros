5fd823fe8c8806d4983ee4009de167266fd68c9a
Robert Strandh <robert.strandh@gmail.com>
2023-06-19 10:42:20 +0200

    Remove obsolete components.

--- Code/common-macros-mop-package.asd
+++
@@ -1,6 +0,0 @@
-(cl:in-package #:asdf-user)
-
-(defsystem #:common-macros-mop-package
-  :serial t
-  :components
-  ((:file "common-macros-mop-package")))
--- Code/common-macros-mop-package.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:common-macros-mop-package
-  (:use)
-  (:export #:ensure-class
-           #:ensure-method
-           #:class-prototype
-           #:generic-function-class-names
-           #:make-method-lambda))
