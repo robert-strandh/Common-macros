ff6cc7b60b22c00237c00dbe88f4be06e6e14573
Robert Strandh <robert.strandh@gmail.com>
2023-07-03 17:48:55 +0200

    Redefine AND in terms of AST transformations.

--- Code/and.lisp
+++ Code/and.lisp
@@ -1,12 +1,18 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:and (&rest forms)
-  (labels ((aux (forms)
-             (if (null (cdr forms))
-                 (car forms)
-                 `(if ,(car forms)
-                      ,(aux (cdr forms))
-                      nil))))
-    (if (null forms)
-        t
-        (aux forms))))
+(defmethod expand ((ast ico:and-ast))
+  (let ((form-asts (ico:form-asts ast))
+        (origin (ico:origin ast)))
+    (abp:with-builder ((make-instance 'bld:builder))
+      (cond ((null form-asts)
+             (abp:node* (:literal :literal 't :source origin)))
+            ((null (rest form-asts))
+             (first form-asts))
+            (t
+             (abp:node* (:if :source origin)
+               (1 :test (first form-asts))
+               (1 :then
+                  (abp:node* (:and :source origin)
+                    (* :form (rest form-asts))))
+               (1 :else
+                  (abp:node* (:literal :literal 'nil :source origin)))))))))
