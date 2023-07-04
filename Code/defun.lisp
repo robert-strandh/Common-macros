2ee42b1ef40a627dcde5a4f943ee2c7e01f7b923
Robert Strandh <robert.strandh@gmail.com>
2023-07-04 10:40:57 +0200

    Fix incorrect count prefix.

--- Code/defun.lisp
+++ Code/defun.lisp
@@ -13,27 +13,29 @@
              )
            (abp:node* (:eval-when)
              (* :situation
-                (abp:node* (:eval-when-situation :situation :load-toplevel))
-                (abp:node* (:eval-when-situation :situation :execute)))
+                (list 
+                 (abp:node* (:eval-when-situation :situation :load-toplevel))
+                 (abp:node* (:eval-when-situation :situation :execute))))
              (* :form
-                (abp:node* (:setf)
-                  (* :place
-                     (abp:node* (:place)
-                       (* :place
-                          (abp:node* (:application)
-                            (1 :function-name
-                               (abp:node* (:function-name :name 'fdefinitions)))
-                            (* :argument
-                               (abp:node* (:quote)
-                                 (1 :object
-                                    (abp:node* (:literal :value name)))))))))
-                  (* :new-value 
-                     (abp:node* (:lambda
-                                  :lambda-list-ast (ico:lambda-list-ast ast)
-                                  :declaration-asts (ico:declaration-asts ast)
-                                  :documentation-ast (ico:documentation-ast ast))
-                       (* :form
-                          (abp:node* (:block :form-asts (ico:form-asts ast))
-                            (1 :block-name
-                               (abp:node* (:block-name :name block-name))))))))
-                (abp:node* (:literal :literal name)))))))))
+                (list 
+                 (abp:node* (:setf)
+                   (1 :place
+                      (abp:node* (:place)
+                        (* :place
+                           (abp:node* (:application)
+                             (1 :function-name
+                                (abp:node* (:function-name :name 'fdefinitions)))
+                             (* :argument
+                                (abp:node* (:quote)
+                                  (1 :object
+                                     (abp:node* (:literal :value name)))))))))
+                   (1 :new-value 
+                      (abp:node* (:lambda
+                                   :lambda-list-ast (ico:lambda-list-ast ast)
+                                   :declaration-asts (ico:declaration-asts ast)
+                                   :documentation-ast (ico:documentation-ast ast))
+                        (1 :form
+                           (abp:node* (:block :form-asts (ico:form-asts ast))
+                             (1 :block-name
+                                (abp:node* (:block-name :name block-name))))))))
+                 (abp:node* (:literal :literal name))))))))))
