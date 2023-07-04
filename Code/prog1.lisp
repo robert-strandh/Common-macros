367c7240cb8c76cb1802c3f4cf73d9f4966635d6
Robert Strandh <robert.strandh@gmail.com>
2023-07-04 10:37:36 +0200

    Redefine PROG1 in terms of AST transformations.

--- Code/prog1.lisp
+++ Code/prog1.lisp
@@ -1,7 +1,18 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:prog1 (first-form &rest forms)
-  (let ((first-form-variable (gensym)))
-    `(let ((,first-form-variable ,first-form))
-       (progn ,@forms
-              ,first-form-variable))))
+(defmethod expand ((ast ico:prog1-ast))
+  (let ((form-asts (ico:form-asts ast))
+        (origin (ico:origin ast))
+        (variable-name (gensym)))
+    (flet ((make-variable-name-ast ()
+             (abp:node* (:variable-name
+                         :source origin
+                         :name variable-name))))
+    (abp:with-builder ((make-instance 'bld:builder))
+      (abp:node* (:let :source origin)
+        (1 :binding
+           (abp:node* (:value-binding :source origin)
+             (1 :name (make-variable-name-ast))
+             (1 :value (first form-asts))))
+        (* :form (rest form-asts))
+        (1 :form (make-variable-name-ast)))))))
