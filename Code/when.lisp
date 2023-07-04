af768e4e936362a75263796aaec3398266c2125f
Robert Strandh <robert.strandh@gmail.com>
2023-07-04 11:40:29 +0200

    Redefine WHEN in terms of AST transformations.

--- Code/when.lisp
+++ Code/when.lisp
@@ -1,5 +1,11 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:when (test &rest forms)
-  `(if ,test
-       (progn ,@forms)))
+(defmethod expand ((ast ico:when-ast))
+  (let ((origin (ico:origin ast)))
+    (abp:with-builder ((make-instance 'builder))
+      (abp:node* (:if :source origin)
+        (1 :test (ico:test-ast ast))
+        (1 :then (abp:node* (:progn :source origin)
+                   (* :form (ico:form-asts ast))))
+        (1 :else
+           (abp:node* (:unparsed :expression 'nil :source origin)))))))
