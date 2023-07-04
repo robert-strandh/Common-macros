2fa431f06d89f4f0a30c93318551ae0639ba9af5
Robert Strandh <robert.strandh@gmail.com>
2023-07-04 15:24:02 +0200

    Fix incorrect use of accessors.

--- Code/prog1.lisp
+++ Code/prog1.lisp
@@ -1,18 +1,17 @@
 (cl:in-package #:common-macros)
 
 (defmethod expand ((ast ico:prog1-ast))
-  (let ((form-asts (ico:form-asts ast))
-        (origin (ico:origin ast))
+  (let ((origin (ico:origin ast))
         (variable-name (gensym)))
     (flet ((make-variable-name-ast ()
              (abp:node* (:variable-name
                          :source origin
                          :name variable-name))))
-    (abp:with-builder ((make-instance 'bld:builder))
-      (abp:node* (:let :source origin)
-        (1 :binding
-           (abp:node* (:value-binding :source origin)
-             (1 :name (make-variable-name-ast))
-             (1 :value (first form-asts))))
-        (* :form (rest form-asts))
-        (1 :form (make-variable-name-ast)))))))
+      (abp:with-builder ((make-instance 'bld:builder))
+        (abp:node* (:let :source origin)
+          (1 :binding
+             (abp:node* (:value-binding :source origin)
+               (1 :name (make-variable-name-ast))
+               (1 :value (ico:first-form-ast ast))))
+          (* :form (form-asts ast))
+          (1 :form (make-variable-name-ast)))))))
