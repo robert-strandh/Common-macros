193ee367480576ec8c97db4182dadf160bcc80fb
Robert Strandh <robert.strandh@gmail.com>
2023-07-04 15:24:18 +0200

    Redefine PROG2 in terms of AST transformations.

--- Code/prog2.lisp
+++ Code/prog2.lisp
@@ -1,8 +1,20 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:prog2 (first-form second-form &rest forms)
-  (let ((second-form-variable (gensym)))
-    `(progn ,first-form
-            (let ((,second-form-variable ,second-form))
-              ,@forms
-              ,second-form-variable))))
+(defmethod expand ((ast ico:prog2-ast))
+  (let ((origin (ico:origin ast))
+        (variable-name (gensym)))
+    (flet ((make-variable-name-ast ()
+             (abp:node* (:variable-name
+                         :source origin
+                         :name variable-name))))
+      (abp:with-builder ((make-instance 'bld:builder))
+        (abp:node* (:progn)
+          (1 :form (ico:first-form-ast ast))
+          (1 :form
+             (abp:node* (:let :source origin)
+               (1 :binding
+                  (abp:node* (:value-binding :source origin)
+                    (1 :name (make-variable-name-ast))
+                    (1 :value (ico:second-form-ast ast))))
+               (* :form (ico:form-asts ast))
+               (1 :form (make-variable-name-ast)))))))))
