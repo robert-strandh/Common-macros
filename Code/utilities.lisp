4a9424bd5bb733cbd092c95ded75b9601cf545c7
Robert Strandh <robert.strandh@gmail.com>
2023-07-05 09:14:36 +0200

    Create AST wrapper around GET-SETF-EXPANSION.

--- Code/utilities.lisp
+++ Code/utilities.lisp
@@ -9,6 +9,8 @@
 ;;; is a syntax error), then those declarations will be considered
 ;;; part of the executable forms.
 
+(defvar *origin*)
+
 (defun separate-ordinary-body (body)
   (let ((pos (position-if (lambda (item)
                             (and (consp item)
@@ -58,3 +60,31 @@
     (1 :name
        (abp:node* (:block-name :source origin :name block-name)))
     (* :form form-asts)))
+
+(defun expand-place-ast (place-ast)
+  (multiple-value-bind
+        (variables value-forms store-variables store-form read-form)
+      (ico:place place-ast)
+    (values
+     (loop for variable in variables
+           for value-form in value-forms
+           for variable-name-ast
+             = (abp:node* (:variable-name :name variable :source *origin*))
+           for value-ast
+             = (abp:node* (:unparsed
+                           :source *origin*
+                           :context :form
+                           :expression value-form))
+           collect (make-let-binding-ast *origin* variable-name-ast value-ast))
+     (loop for store-variable in store-variables
+           collect
+           (abp:node* (:variable-name :name store-variable :source *origin*)))
+     (abp:node* (:unparsed
+                 :source *origin*
+                 :context :form
+                 :expression store-form))
+     (abp:node* (:unparsed
+                 :source *origin*
+                 :context :form
+                 :expression read-form)))))
+           
