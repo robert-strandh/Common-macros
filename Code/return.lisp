ae12c05a93a2c9233bf2631005306334725dcb35
Robert Strandh <robert.strandh@gmail.com>
2023-07-05 06:00:04 +0200

    Redefine RETURN in terms of AST transformations.

--- Code/return.lisp
+++ Code/return.lisp
@@ -1,4 +1,8 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:return (form)
-  `(return-from nil ,form))
+(defmethod expand ((ast ico:return-ast))
+  (let ((origin (ico:origin ast)))
+    (abp:with-builder ((make-instance 'bld:builder))
+      (abp:node* (:return-from :source origin)
+        (1 :name (abp:node* (:block-name :source origin :name 'nil)))
+        (? :result (ico:result-ast ast))))))
