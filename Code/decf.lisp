0401487e7e0a819c5031e35fc7cea2588f4f4c4b
Robert Strandh <robert.strandh@gmail.com>
2023-07-05 18:02:35 +0200

    Use new macro EXPAND-PLACE-AST to simplify code.

--- Code/decf.lisp
+++ Code/decf.lisp
@@ -1,51 +1,24 @@
 (cl:in-package #:common-macros)
 
 (defmethod expand ((ast ico:decf-ast) environment)
-  (let ((place (ico:place (ico:place-ast ast))))
-    (with-builder
-      (multiple-value-bind (vars vals store-vars writer-form reader-form)
-          (get-setf-expansion place environment)
-        (let* ((variable-name-ast
-                 (node* (:variable-name
-                         :name (first store-vars))))
-               (delta-ast
-                 (if (null (ico:delta-ast ast))
-                     (node* (:unparsed
-                             :context :form
-                             :expression '1))
-                     (ico:delta-ast ast)))
-               (reader-form-ast
-                 (node* (:unparsed
-                         :context :form
-                         :expression reader-form)))
-               (writer-form-ast
-                 (node* (:unparsed
-                         :context :form
-                         :expression writer-form)))
-               (application-ast
-                 (node* (:application)
-                   (1 :function-name
-                      (node* (:function-name :name '-)))
-                   (1 :argument reader-form-ast)
-                   (1 :argument delta-ast)))
-               (let-binding-ast
-                 (make-let-binding-ast variable-name-ast application-ast)))
-          (if (null vars)
-              (node* (:let)
-                (1 :binding let-binding-ast)
-                (1 :form writer-form-ast))
-              (node* (:let)
-                (1 :binding let-binding-ast)
-                (* :binding
-                   (loop for var in vars
-                         for val in vals
-                         for variable-name-ast
-                           = (node* (:variable-name
-                                     :name var))
-                         for value-ast
-                           = (node* (:unparsed
-                                     :context :form
-                                     :expression val))
-                         collect
-                         (make-let-binding-ast variable-name-ast value-ast)))
-                (1 :form writer-form-ast))))))))
+  (with-builder
+    (multiple-value-bind
+          (binding-asts store-variable-asts store-ast read-ast)
+        (expand-place-ast (ico:place-ast ast))
+      (let* ((variable-name-ast (first store-variable-asts))
+             (delta-ast
+               (if (null (ico:delta-ast ast))
+                   (node* (:unparsed :context :form :expression '1))
+                   (ico:delta-ast ast)))
+             (application-ast
+               (node* (:application)
+                 (1 :function-name
+                    (node* (:function-name :name '-)))
+                 (1 :argument read-ast)
+                 (1 :argument delta-ast)))
+             (let-binding-ast
+               (make-let-binding-ast variable-name-ast application-ast)))
+        (node* (:let)
+          (1 :binding let-binding-ast)
+          (* :binding binding-asts)
+          (1 :form store-ast))))))
