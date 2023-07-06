a1ee40ea005ed51b7e95be7560a9a2689e7115f9
Robert Strandh <robert.strandh@gmail.com>
2023-07-06 12:36:11 +0200

    Redefine IN-PACKAGE in terms of AST transformations.

--- Code/in-package.lisp
+++ Code/in-package.lisp
@@ -1,5 +1,18 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:in-package (name)
-  `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (setq *package* (find-package ',name))))
+(defmethod expand (client (ast ico:in-package-ast) environment)
+  (declare (ignore client environment))
+  (node* (:eval-when)
+    (* :situation
+       (make-eval-when-situation-asts
+        :compile-toplevel :load-toplevel :execute))
+    (1 :form
+       (node* (:setq)
+         (1 :variable-name (make-variable-name-ast '*package*))
+         (1 :value
+            (node* (:application)
+              (1 :function-name
+                 (node* (:function-name :name 'find-package)))
+              (1 :argument
+                 (node* (:quote)
+                   (1 :object (ico:name (ico:name-ast ast)))))))))))
