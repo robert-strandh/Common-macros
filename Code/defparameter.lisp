198ea93a987e472f5f77632348103fdaa174330a
Robert Strandh <robert.strandh@gmail.com>
2023-07-06 10:07:59 +0200

    Redefine DEFPARAMETER in terms of AST transformations.

--- Code/defparameter.lisp
+++ Code/defparameter.lisp
@@ -1,11 +1,34 @@
 (cl:in-package #:common-macros)
 
-;;; This code is taken from the dictionary page on DEFPARAMETER in the
+;;; This code is inspred by the dictionary page on DEFPARAMETER in the
 ;;; Common Lisp standard.
-(defmacro cmd:defparameter
-    (name initial-value &optional (documentation nil documentation-p))
-  `(progn (declaim (special ,name))
-          (setf (symbol-value ',name) ,initial-value)
-          ,(when documentation-p
-             `(setf (documentation ',name 'variable) ',documentation))
-          ',name))
+
+(defmethod expand (client (ast ico:defparameter-ast) environment)
+  (declare (ignore client environment))
+  (let ((variable-name-ast (ico:variable-name-ast ast))
+        (form-ast (ico:form-ast ast))
+        (documentation-ast (ico:documentation-ast ast)))
+    (node* (:progn)
+      (1 :form
+         (node* (:declaim)
+           (1 :declaration-specifier
+              (node* (:declaration-specifier :kind 'special)
+                (1 :argument variable-name-ast)))))
+      (1 :form
+         (node* (:setf)
+           (1 :name variable-name-ast)
+           (1 :value form-ast)))
+      (abp:? :form
+             (if (null documentation-ast)
+                 nil
+                 (node* (:setf)
+                   (1 :place
+                      (node* (:place)
+                        (1 :place
+                           (node* (:application)
+                             (1 :function-name
+                                (node* (:function-name :name 'documentation)))
+                             (1 :argument variable-name-ast)
+                             (1 :argument (node* (:quote :object 'variable)))))))
+                   (1 :value documentation-ast))))
+      (1 :form variable-name-ast))))
