e81b81219b01a668594b730341c09314b2853aa5
Robert Strandh <robert.strandh@gmail.com>
2023-07-06 08:31:16 +0200

    Redefine PUSH in terms of AST transformations.

--- Code/push.lisp
+++ Code/push.lisp
@@ -1,10 +1,21 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:push (item place &environment environment)
-  (multiple-value-bind (vars vals store-vars writer-form reader-form)
-      (get-setf-expansion place environment)
-    (let ((item-var (gensym)))
-      `(let* ((,item-var ,item)
-              ,(mapcar #'list vars vals)
-              (,(car store-vars) (cons ,item-var ,reader-form)))
-         ,writer-form))))
+(defmethod expand ((ast ico:push-ast) environment)
+  (with-builder
+    (multiple-value-bind
+          (binding-asts store-variable-asts store-ast read-ast)
+        (expand-place-ast (ico:place-ast ast))
+      (let ((item-var (gensym)))
+        (node* (:let*)
+          (1 :binding
+             (make-let-binding-ast
+              (make-variable-name-ast item-var) (ico:item-ast ast)))
+          (* :binding binding-asts)
+          (1 :binding
+             (make-let-binding-ast
+              (first store-variable-asts)
+              (node* (:application)
+                (1 :function-name (node* (:function-name :name 'cons)))
+                (1 :argument (make-variable-name-ast item-var))
+                (1 :argument read-ast))))
+          (1 :form store-ast))))))
