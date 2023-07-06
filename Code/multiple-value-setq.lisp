cb1670345a55d63d7db2d5367ad0eb1496c96188
Robert Strandh <robert.strandh@gmail.com>
2023-07-06 11:56:51 +0200

    Mark MULTIPLE-VALUE-SETQ as using EXPAND.

--- Code/multiple-value-setq.lisp
+++ Code/multiple-value-setq.lisp
@@ -1,4 +1,15 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:multiple-value-setq (symbols form)
-  `(values (setf (values ,@symbols) ,form)))
+(defmethod expand (client (ast ico:multiple-value-setq-ast) environment)
+  (declare (ignore client environment))
+  (node* (:application)
+    (1 :function-name (node* (:function-name :name 'values)))
+    (1 :argument
+       (node* (:setf)
+         (1 :place
+            (node* (:place)
+              (1 :place
+                 (node* (:appliication)
+                   (1 :function-name (node* (:function-name :name 'values)))
+                   (* :argument (ico:name-asts ast))))
+              (1 :form (ico:form-ast ast))))))))
