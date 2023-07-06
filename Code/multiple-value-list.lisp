40131af210792bd349ea073a5b0b059cd28c24bf
Robert Strandh <robert.strandh@gmail.com>
2023-07-06 10:59:45 +0200

    Redefine MULTIPLE-VALUE-LIST in terms of AST transformations.

--- Code/multiple-value-list.lisp
+++ Code/multiple-value-list.lisp
@@ -1,4 +1,9 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:multiple-value-list (form)
-  `(multiple-value-call #'list ,form))
+(defmethod expand (client (ast ico:multiple-value-list-ast) environment)
+  (declare (ignore client environment))
+  (node* (:application)
+    (1 :function-name (node* (:function-name :name 'multiple-value-call)))
+    (1 :argument
+       (node* (:function) (1 :name (node* (:function-name :name 'list)))))
+    (1 :argument (ico:form-ast ast))))
