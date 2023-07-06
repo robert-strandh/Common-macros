ffbe4c6e1b90c8eb11cead33ca2adab766cd7e49
Robert Strandh <robert.strandh@gmail.com>
2023-07-06 16:54:20 +0200

    Redefine PSETQ in terms of AST transformations.

--- Code/psetq.lisp
+++ Code/psetq.lisp
@@ -1,4 +1,9 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:psetq (&rest pairs)
-  `(psetf ,@pairs))
+(defmethod expand (client (ast ico:psetq-ast) environment)
+  (declare (ignore client environment))
+  (node* (:psetf)
+    (* :place
+       (loop for variable-name-ast in (ico:variable-name-asts ast)
+             collect (node* (:place) (1 :place variable-name-ast))))
+    (* :value (ico:value-asts ast))))
