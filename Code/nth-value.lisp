0406d14a9884c3e7cc7ef2291b54b563ef6a8cba
Robert Strandh <robert.strandh@gmail.com>
2023-07-06 06:06:26 +0200

    Redefine NTH-VALUE in terms of AST transformations.

--- Code/nth-value.lisp
+++ Code/nth-value.lisp
@@ -1,4 +1,13 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:nth-value (n form)
-  `(nth ,n (multiple-value-list ,form)))
+(defmethod expand ((ast ico:nth-value-ast) environment)
+  (declare (ignore environment))
+  (with-builder
+    (node* (:application)
+      (1 :function-name (node* (:function-name :name 'nth)))
+      (1 :argument (ico:n-ast ast))
+      (1 :argument
+         (node* (:application)
+           (1 :function-name
+              (node* (:function-name :name 'multiple-value-list)))
+           (1 :argument (ico:form-ast ast)))))))
