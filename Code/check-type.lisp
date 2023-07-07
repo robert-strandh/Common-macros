5aa295e1d6f25b07b831dc7741b1bfbabe8f6cee
Robert Strandh <robert.strandh@gmail.com>
2023-07-07 15:47:49 +0200

    Redefine CHECK-TYPE in terms of AST transformations.

--- Code/check-type.lisp
+++ Code/check-type.lisp
@@ -2,7 +2,22 @@
 
 ;;; This definition is from the Common Lisp standard.  The STRING
 ;;; argument is not used.
-(defmacro cmd:check-type (place typespec string)
-  (declare (ignore string))
-  `(assert (typep ,place ',typespec) (,place)
-           'type-error :datum ,place :expected-type ',typespec))
+(defmethod expand (client (ast ico:check-type-ast) environment)
+  (node* (:assert)
+    (1 :test-form
+       (node* (:application)
+         (1 :function-name (make-function-name-ast 'typep))
+         (1 :argument (ico:place-ast ast))
+         (1 :argument
+            (make-quote-ast
+             (ses:unparse
+              (make-instance 'bld:builder) t (ico:typespec-ast ast))))))
+    (1 :place (ico:place-ast ast))
+    (1 :datum (make-quote-ast 'type-error))
+    (1 :argument (make-unparsed-form-ast ':datum))
+    (1 :argument (ico:place-ast ast))
+    (1 :argument (make-unparsed-form-ast ':expected-type))
+    (1 :argument 
+       (make-quote-ast
+        (ses:unparse
+         (make-instance 'bld:builder) t (ico:typespec-ast ast))))))
