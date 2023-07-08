ad3a8127364da543fdfd2a32552f22387049533f
Robert Strandh <robert.strandh@gmail.com>
2023-07-08 15:13:58 +0200

    Redefine DOTIMES in terms of AST transformations.

--- Code/dotimes.lisp
+++ Code/dotimes.lisp
@@ -6,23 +6,43 @@
 ;;; variable in each iteration.  We have chosen to bind the variable
 ;;; once for the entire loop body.
 
-(defmacro cmd:dotimes ((var count-form &optional result-form) &body body)
-  (multiple-value-bind (declarations forms)
-      (separate-ordinary-body body)
-    (let ((start-tag (gensym))
-          (end-tag (gensym))
-          (count-var (gensym)))
-      `(let ((,count-var ,count-form)
-             (,var 0))
-         (declare (type unsigned-byte ,var))
-         ,@declarations
-         (block nil
-           (tagbody
-              ,start-tag
-              (when (= ,var ,count-var)
-                (go ,end-tag))
-              (tagbody ,@forms)
-              (incf ,var)
-              (go ,start-tag)
-              ,end-tag)
-           ,result-form)))))
+(defmethod expand (client (ast ico:dotimes-ast) environment)
+  (declare (ignore client environment))
+  (let ((start-tag (gensym))
+        (end-tag (gensym))
+        (count-var (gensym)))
+    (node* (:let)
+      (1 :binding
+         (make-let-binding-ast
+          (make-variable-name-ast count-var)
+          (ico:count-form-ast ast)))
+      (1 :binding
+         (make-let-binding-ast
+          (ico:var-ast ast)
+          (make-unparsed-form-ast '0)))
+      (* :declaration (ico:declaration-asts ast))
+      (wrap-in-block-ast
+       nil
+       (node* (:tagbody)
+         (1 :segment
+            (node* (:segment)
+              (1 :tag (node* (:tag :name start-tag)))
+              (1 :statement
+                 (node* (:when)
+                   (1 :test
+                      (node* (:application)
+                        (1 :function-name (make-function-name-ast '=))
+                        (1 :argument (ico:var-ast ast))
+                        (1 :argument (make-variable-name-ast count-var))))
+                   (1 :form
+                      (node* (:go) (1 :tag (node* (:tag :name end-tag)))))))
+              (1 :statement
+                 (node* (:tagbody) (* :segment (ico:segment-asts ast))))
+              (1 :statement
+                 (node* (:incf) (1 :place (ico:var-ast ast))))
+              (1 :statement
+                 (node* (:go) (1 :tag (node* (:tag :name end-tag)))))))
+         (1 :segment
+            (node* (:segment)
+              (1 :tag (node* (:tag :name end-tag))))))
+       (ico:result-ast ast)))))
