421bd892a89663613f14c357dfc40ae92aecf988
Robert Strandh <robert.strandh@gmail.com>
2023-07-09 14:15:37 +0200

    Fix incorrect relation.

--- Code/with-slots.lisp
+++ Code/with-slots.lisp
@@ -5,7 +5,7 @@
     (alet ((b (make-variable-name-ast instance-var)
               (ico:instance-form-ast ast)))
       (node* (:symbol-macrolet)
-        (* :symbol-expansion-asts
+        (* :symbol-expansion
            (loop for slot-entry-ast in (ico:slot-entry-asts ast)
                  for slot-name-ast
                    = (ico:slot-name-ast slot-entry-ast)
