f7a339cb2465816ce7e6f1d34ab7b1adc2d47e83
Robert Strandh <robert.strandh@gmail.com>
2023-07-09 10:01:37 +0200

    Use new macro APPLICATION to simplify code.

--- Code/decf.lisp
+++ Code/decf.lisp
@@ -5,16 +5,11 @@
   (multiple-value-bind
         (binding-asts store-variable-asts store-ast read-ast)
       (expand-place-ast (ico:place-ast ast) environment)
-    (let* ((variable-name-ast (first store-variable-asts))
-           (delta-ast
-             (if (null (ico:delta-ast ast))
-                 (make-unparsed-form-ast '1)
-                 (ico:delta-ast ast)))
-           (application-ast
-             (node* (:application)
-               (1 :function-name (make-function-name-ast '-))
-               (1 :argument read-ast)
-               (1 :argument delta-ast))))
-      (alet ((b variable-name-ast application-ast)
+    (let ((delta-ast
+            (if (null (ico:delta-ast ast))
+                (make-unparsed-form-ast '1)
+                (ico:delta-ast ast))))
+      (alet ((b (first store-variable-asts)
+                (application '- read-ast delta-ast))
              binding-asts)
         store-ast))))
