4ea4b7d936c8dc66cea1d605dbb34ab389c612bb
Robert Strandh <robert.strandh@gmail.com>
2023-07-10 11:14:48 +0200

    Add test cases with array for DECF.

--- Code/Test/decf.lisp
+++ Code/Test/decf.lisp
@@ -23,3 +23,17 @@
         1
         (eval `(let ((x 3))
                  ,(expand-expression '(decf x 2))))))
+
+(define-test decf-array-default-delta
+  :parent decf
+    (is #'eql
+        1
+        (eval `(let ((x (make-array 1 :initial-element 2)))
+                 ,(expand-expression '(decf (aref x 0)))))))
+
+(define-test decf-array-with-delta
+  :parent decf
+    (is #'eql
+        1
+        (eval `(let ((x (make-array 1 :initial-element 3)))
+                 ,(expand-expression '(decf (aref x 0) 2))))))
