56f4f9ee15def8a816ee69a9669b6bac056d21e2
Robert Strandh <robert.strandh@gmail.com>
2023-07-10 17:02:48 +0200

    Fix bad indentation.

--- Code/Test/decf.lisp
+++ Code/Test/decf.lisp
@@ -4,36 +4,36 @@
 
 (define-test decf-variable-default-delta
   :parent decf
-    (is #'eql
-        1
-        (eval `(let ((x 2))
-                 ,(expand-expression '(decf x))))))
+  (is #'eql
+      1
+      (eval `(let ((x 2))
+               ,(expand-expression '(decf x))))))
 
 (define-test decf-variable-default-delta-side-effect
   :parent decf
-    (is #'eql
-        1
-        (eval `(let ((x 2))
-                 ,(expand-expression '(decf x))
-                 x))))
+  (is #'eql
+      1
+      (eval `(let ((x 2))
+               ,(expand-expression '(decf x))
+               x))))
 
 (define-test decf-variable-with-delta
   :parent decf
-    (is #'eql
-        1
-        (eval `(let ((x 3))
-                 ,(expand-expression '(decf x 2))))))
+  (is #'eql
+      1
+      (eval `(let ((x 3))
+               ,(expand-expression '(decf x 2))))))
 
 (define-test decf-array-default-delta
   :parent decf
-    (is #'eql
-        1
-        (eval `(let ((x (make-array 1 :initial-element 2)))
-                 ,(expand-expression '(decf (aref x 0)))))))
+  (is #'eql
+      1
+      (eval `(let ((x (make-array 1 :initial-element 2)))
+               ,(expand-expression '(decf (aref x 0)))))))
 
 (define-test decf-array-with-delta
   :parent decf
-    (is #'eql
-        1
-        (eval `(let ((x (make-array 1 :initial-element 3)))
-                 ,(expand-expression '(decf (aref x 0) 2))))))
+  (is #'eql
+      1
+      (eval `(let ((x (make-array 1 :initial-element 3)))
+               ,(expand-expression '(decf (aref x 0) 2))))))
