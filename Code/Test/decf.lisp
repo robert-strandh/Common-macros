21ae59bd81e5c432830b06ed9bb78751eafb9041
Robert Strandh <robert.strandh@gmail.com>
2023-07-10 10:54:34 +0200

    Add more tests for DECF.

--- Code/Test/decf.lisp
+++ Code/Test/decf.lisp
@@ -8,3 +8,18 @@
         1
         (eval `(let ((x 2))
                  ,(expand-expression '(decf x))))))
+
+(define-test decf-variable-default-delta-side-effect
+  :parent decf
+    (is #'eql
+        1
+        (eval `(let ((x 2))
+                 ,(expand-expression '(decf x))
+                 x))))
+
+(define-test decf-variable-with-delta
+  :parent decf
+    (is #'eql
+        1
+        (eval `(let ((x 3))
+                 ,(expand-expression '(decf x 2))))))
