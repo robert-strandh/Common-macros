62c09a3fe15600f81cab2815fd619090112f9144
Robert Strandh <robert.strandh@gmail.com>
2023-07-11 08:47:58 +0200

    Define tests for update behavior of DEFVAR and DEFPARAMETER.

--- Code/Test/defparameter.lisp
+++ Code/Test/defparameter.lisp
@@ -2,9 +2,13 @@
 
 (define-test defparameter)
 
-(define-test defparameter-no-documentation
+(define-test defparameter-check-update
   :parent defparameter
   (let ((name (gensym)))
     (is #'equal
-        (eval `(progn (defparameter ,name 234) ,name))
-        (eval `(progn ,(expand-expression `(defparameter ,name)) ,name)))))
+        (eval `(progn (defparameter ,name 234)
+                      (defparameter ,name 345)
+                      ,name))
+        (eval `(progn
+                 (defparameter ,name 234)
+                 ,(expand-expression `(defparameter ,name 345)) ,name)))))
--- Code/Test/defvar.lisp
+++ Code/Test/defvar.lisp
@@ -16,3 +16,14 @@
     (is #'equal
         (eval `(progn (defvar ,name 234) ,name))
         (eval `(progn ,(expand-expression `(defvar ,name)) ,name)))))
+
+(define-test defvar-check-update
+  :parent defvar
+  (let ((name (gensym)))
+    (is #'equal
+        (eval `(progn (defvar ,name 234)
+                      (defvar ,name 345)
+                      ,name))
+        (eval `(progn
+                 (defvar ,name 234)
+                 ,(expand-expression `(defvar ,name 345)) ,name)))))
