10431e65be50ca7d0debce96b6aefef24d227aff
Robert Strandh <robert.strandh@gmail.com>
2023-07-12 16:39:32 +0200

    Uncomment code that now works.

--- Code/Test/with-slots.lisp
+++ Code/Test/with-slots.lisp
@@ -8,17 +8,14 @@
         #1=(with-slots () #'print-object)
         (eval (expand-expression #1#))))
 
-;;; This form fails to compile so it is commented out until
-;;; it can be determined whether Parachute has a bug.
-
-;; (define-test with-slots-empty
-;;   :parent with-slots
-;;   (let ((class-name (gensym))
-;;         (slot-name (gensym)))
-;;     (is #'equal
-;;         (eval `(progn #2=(defclass ,class-name () ((,slot-name)))
-;;                       #1=(with-slots ((,slot-name ,slot-name))
-;;                              (make-instance ',class-name)
-;;                            (setq ,slot-name 234)
-;;                            ,slot-name)))
-;;         (eval `(progn #2# ,(expand-expression #1#))))))
+(define-test with-slots-empty
+  :parent with-slots
+  (let ((class-name (gensym))
+        (slot-name (gensym)))
+    (is #'equal
+        (eval `(progn #2=(defclass ,class-name () ((,slot-name)))
+                      #1=(with-slots ((,slot-name ,slot-name))
+                             (make-instance ',class-name)
+                           (setq ,slot-name 234)
+                           ,slot-name)))
+        (eval `(progn #2# ,(expand-expression '#1#))))))
