6a7753d84448566736d6bdab9961b11f58acfdfc
Robert Strandh <robert.strandh@gmail.com>
2023-07-12 10:03:02 +0200

    Use reader macros to avoid repeated forms for testing PROG1, PROG2.

--- Code/Test/prog1.lisp
+++ Code/Test/prog1.lisp
@@ -5,13 +5,13 @@
 (define-test prog1-only-first-form
   :parent prog1
     (is #'eql
-        (prog1 234)
-        (eval (expand-expression `(prog1 234)))))
+        #1=(prog1 234)
+        (eval (expand-expression `#1#))))
 
 (define-test prog1-one-additional-form
   :parent prog1
     (is #'equal
-        (let ((x 0)) (list (prog1 234 (setq x 1)) x))
-        (eval `(let ((x 0))
-                 (list ,(expand-expression `(prog1 234 (setq x 1))) x)))))
+        (let #2=((x 0)) (list #1=(prog1 234 (setq x 1)) x))
+        (eval `(let #2#
+                 (list ,(expand-expression `#1#) x)))))
 
--- Code/Test/prog2.lisp
+++ Code/Test/prog2.lisp
@@ -5,15 +5,15 @@
 (define-test prog2-only-first-and-second-form
   :parent prog2
     (is #'eql
-        (prog2 234 345)
-        (eval (expand-expression `(prog2 234 345)))))
+        #1=(prog2 234 345)
+        (eval (expand-expression `#1#))))
 
 (define-test prog2-one-additional-form
   :parent prog2
     (is #'equal
-        (let ((x 0)) (list (prog2 234 345 (setq x 1)) x))
-        (eval `(let ((x 0))
+        (let #2=((x 0)) (list #1=(prog2 234 345 (setq x 1)) x))
+        (eval `(let #2#
                  (list ,(expand-expression
-                         `(prog2 234 345 (setq x 1)))
+                         '#1#)
                        x)))))
 
