d84bdfca525efb49b76366a456d2cb691e92c462
Robert Strandh <robert.strandh@gmail.com>
2023-07-12 08:23:42 +0200

    Fix incorect AST accessor name.

--- Code/dotimes.lisp
+++ Code/dotimes.lisp
@@ -12,17 +12,17 @@
         (end-tag (gensym))
         (count-var (gensym)))
     (alet ((b (make-variable-name-ast count-var) (ico:count-form-ast ast))
-           (b (ico:var-ast ast) (make-unparsed-form-ast '0)))
+           (b (ico:variable-name-ast ast) (make-unparsed-form-ast '0)))
       (ico:declaration-asts ast)
       (ablock 'nil
         (atagbody
          (atag start-tag)
          (awhen 
-             (application '= (ico:var-ast ast)
+             (application '= (ico:variable-name-ast ast)
                           (make-variable-name-ast count-var))
            (ago (atag end-tag)))
          (node* (:tagbody) (* :segment (ico:segment-asts ast)))
-         (node* (:incf) (1 :place (ico:var-ast ast)))
+         (node* (:incf) (1 :place (ico:variable-name-ast ast)))
          (ago (atag start-tag))
          (atag end-tag)))
       (ico:result-ast ast))))
