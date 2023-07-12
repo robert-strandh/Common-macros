a3d19c0e7ec87d83e862b95ddb7d2bd875c5ee0a
Robert Strandh <robert.strandh@gmail.com>
2023-07-12 09:09:59 +0200

    Use reader macros to avoid repeated forms.

--- Code/Test/dolist.lisp
+++ Code/Test/dolist.lisp
@@ -5,18 +5,18 @@
 (define-test dolist-empty-no-result-form
   :parent dolist
   (is #'equal
-      (dolist (var '()))
-      (eval (expand-expression '(dolist (var '()))))))
+      #1=(dolist (var '()))
+      (eval (expand-expression '#1#))))
 
 (define-test dolist-empty-with-result-form
   :parent dolist
   (is #'equal
-      (dolist (var '() 234))
-      (eval (expand-expression '(dolist (var '() 234))))))
+      #1=(dolist (var '() 234))
+      (eval (expand-expression '#1#))))
 
 (define-test dolist-one-element-with-result-form
   :parent dolist
   (is #'equal
-      (let ((x 0)) (dolist (var '(1) x) (setq x var)))
+      (let ((x 0)) #1=(dolist (var '(1) x) (setq x var)))
       (eval `(let ((x 0))
-               ,(expand-expression '(dolist (var '(1) x) (setq x var)))))))
+               ,(expand-expression '#1#)))))
