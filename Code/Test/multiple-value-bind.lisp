456d2ee04eb0515d2aed6c037bbc4c5b4ca6c995
Robert Strandh <robert.strandh@gmail.com>
2023-07-12 09:10:20 +0200

    Add more test cases for MULTIPLE-VALUE-BIND.

--- Code/Test/multiple-value-bind.lisp
+++ Code/Test/multiple-value-bind.lisp
@@ -5,16 +5,30 @@
 (define-test multiple-value-bind-empty
   :parent multiple-value-bind
   (is #'eql
-      (multiple-value-bind () 234)
-      (expand-expression '(multiple-value-bind () 234))))
+      #1=(multiple-value-bind () 234)
+      (expand-expression '#1#)))
 
 (define-test multiple-value-bind-no-vars-side-effect
   :parent multiple-value-bind
   :depends-on (multiple-value-bind-empty)
   (is #'eql
       (let ((x 10))
-        (multiple-value-bind () (setq x 11))
+        #1=(multiple-value-bind () (setq x 11))
         x)
       (eval `(let ((x 10))
-               ,(expand-expression '(multiple-value-bind () (setq x 11)))
+               ,(expand-expression '#1#)
                x))))
+
+(define-test multiple-value-bind-one-var
+  :parent multiple-value-bind
+  :depends-on (multiple-value-bind-empty)
+  (is #'eql
+      #1=(multiple-value-bind (x) 234 x)
+      (eval (expand-expression '#1#))))
+
+(define-test multiple-value-bind-two-vars-one-value
+  :parent multiple-value-bind
+  :depends-on (multiple-value-bind-empty)
+  (is #'equal
+      #1=(multiple-value-bind (x y) 234 (list x y))
+      (eval (expand-expression '#1#))))
