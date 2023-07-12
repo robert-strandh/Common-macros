703170f2f3baeb137a94b0cab57aeb4cd6243a43
Robert Strandh <robert.strandh@gmail.com>
2023-07-12 09:46:20 +0200

    Use reader macros to avoid repeated forms for testing POP.

--- Code/Test/pop.lisp
+++ Code/Test/pop.lisp
@@ -5,27 +5,27 @@
 (define-test pop-with-one-element
   :parent pop
   (is #'equal
-      (let ((x '(1))) (pop x))
-      (eval `(let ((x '(1)))
-               ,(expand-expression '(pop x))))))
+      (let #2=((x '(1))) #1=(pop x))
+      (eval `(let #2#
+               ,(expand-expression '#1#)))))
 
 (define-test pop-with-one-element-side-effect
   :parent pop
   (is #'equal
-      (let ((x '(1))) (pop x) x)
-      (eval `(let ((x '(1)))
-               ,(expand-expression '(pop x)) x))))
+      (let #2=((x '(1))) #1=(pop x) x)
+      (eval `(let #2#
+               ,(expand-expression '#1#) x))))
 
 (define-test pop-with-two-elements
   :parent pop
   (is #'equal
-      (let ((x '(1 2))) (pop x))
-      (eval `(let ((x '(1 2)))
-               ,(expand-expression '(pop x))))))
+      (let #2=((x '(1 2))) #1=(pop x))
+      (eval `(let #2#
+               ,(expand-expression '#1#)))))
 
 (define-test pop-with-two-elements-side-effect
   :parent pop
   (is #'equal
-      (let ((x '(1 2))) (pop x) x)
-      (eval `(let ((x '(1 2)))
-               ,(expand-expression '(pop x)) x))))
+      (let #2=((x '(1 2))) #1=(pop x) x)
+      (eval `(let #2#
+               ,(expand-expression '#1#) x))))
