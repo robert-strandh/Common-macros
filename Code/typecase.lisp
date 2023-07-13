07b63d0fea8d5476e8676808520cc10b2f9cf7db
Robert Strandh <robert.strandh@gmail.com>
2023-07-13 09:59:34 +0200

    Define macro CASE as an AST transformer.

--- Code/typecase.lisp
+++ Code/typecase.lisp
@@ -1,24 +1,25 @@
 (cl:in-package #:common-macros)
 
-;;;; This definition is temporary, and it does very little
-;;;; error checking.  It should be replaced by a definition
-;;;; that uses Iconoclast.
-
-(defun expand-typecase-clauses (clauses key-variable)
-  (if (null clauses)
-      'nil
-      (let ((clause (car clauses)))
-        (if (or (eq (car clause) 'otherwise)
-                (eq (car clause) t))
-            `(progn ,@(cdr clause))
-            (let ((type (car clause))
-                  (forms (cdr clause)))
-              `(if (typep ,key-variable ',type)
-                   (progn ,@forms)
-                   ,(expand-typecase-clauses
-                     (cdr clauses) key-variable)))))))
-
-(defmacro cmd:typecase (keyform &rest clauses)
-  (let ((key-variable (gensym)))
-    `(let ((,key-variable ,keyform))
-       ,(expand-typecase-clauses clauses key-variable))))
+(defmethod expand (client (ast ico:typecase-ast) environment)
+  (declare (ignore client environment))
+  (let ((variable-name (gensym))
+        ;; FIXME: store the builder in a special variable.
+        (builder (make-instance 'bld:builder)))
+    (flet ((make-cond-clause-ast (typecase-clause-ast)
+             (if (typep typecase-clause-ast 'ico:typecase-normal-clause-ast)
+                 (let* ((typespec-ast (ico:typespec-ast typecase-clause-ast))
+                        (typespec (ses:unparse builder t typespec-ast)))
+                   (node* (:cond-clause)
+                     (1 :test
+                        (application
+                         'typep
+                         (make-variable-name-ast variable-name)
+                         (node* (:quote :object typespec))))
+                     (* :form (ico:form-asts typecase-clause-ast))))
+                 (node* (:cond-clause)
+                   (1 :test (node* (:quote :object t)))
+                   (* :form (ico:form-asts typecase-clause-ast))))))
+      (alet ((b (make-variable-name-ast variable-name)
+                (ico:keyform-ast ast)))
+        (node* (:cond)
+          (* :clause (mapcar #'make-cond-clause-ast (ico:clause-asts ast))))))))
