fe760f5f4bd6f5fcf43d32e255d1d4b0fcf1a30d
Robert Strandh <robert.strandh@gmail.com>
2023-07-14 14:29:34 +0200

    Define macro WITH-INPUT-FROM-STRING as an AST transformer.

--- Code/with-input-from-string.lisp
+++ Code/with-input-from-string.lisp
@@ -3,25 +3,35 @@
 ;;; This definition does not quite work, because it uses a
 ;;; non-standard function STRING-INPUT-STREAM-POSITION.
 
-(defmacro cmd:with-input-from-string
-    ((string-stream-variable
-      string
-      &key
-        (start 0) end index)
-     &body body)
-  (if (null index)
-      (multiple-value-bind (declarations forms)
-          (separate-ordinary-body body)
-        `(with-open-stream
-             (,string-stream-variable
-              (make-string-input-stream ,string ,start ,end))
-           ,@declarations
-           (multiple-value-prog1
-               (progn ,@forms)
-             (setf ,index
-                   (string-input-stream-position
-                    ,string-stream-variable)))))
-      `(with-open-stream
-           (,string-stream-variable
-            (make-string-input-stream ,string ,start ,end))
-         ,@body)))
+(defmethod expand (client (ast ico:with-input-from-string-ast) environment)
+  (declare (ignore client environment))
+  (let ((make-string-input-stream-ast
+          (application
+           'make-string-input-stream
+           (if (null (ico:start-ast ast))
+               (node* (:literal :literal 0))
+               (ico:start-ast ast))
+           (if (null (ico:end-ast ast))
+               (node* (:literal :literal 'nil))
+               (ico:end-ast ast)))))
+  (if (null (ico:index-ast ast))
+      (node* (:with-open-stream)
+        (1 :var (ico:var-ast ast))
+        (1 :stream make-string-input-stream-ast)
+        (* :form (ico:form-asts ast)))
+      (node* (:with-open-stream)
+        (1 :var (ico:var-ast ast))
+        (1 :stream make-string-input-stream-ast)
+        (* :declaration (ico:declaration-asts ast))
+        (* :form
+           (node* (:multiple-value-prog1)
+             (1 :values
+                (node* (:progn)
+                  (* :form (ico:form-asts ast))))
+             (1 :form
+                (node* (:setf)
+                  (1 :place (ico:index-ast ast))
+                  (1 :value
+                     (application
+                      'string-input-stream-position
+                      (ico:var-ast ast)))))))))))
