e78f11183eb028f30a8dcd71501316cce2df5953
Robert Strandh <robert.strandh@gmail.com>
2023-07-14 16:06:46 +0200

    Define macro DEFCLASS as an AST transformer.

--- Code/defclass.lisp
+++ Code/defclass.lisp
@@ -1,61 +1,98 @@
 (cl:in-package #:common-macros)
 
-;;; FIXME: Include :TYPE slot option in canonical slot specifier.
+;;; FIXME: handle ALLOCATION, TYPE, and implementation-defined
+;;; options.
+(defun canonicalize-slot-specifier-ast (slot-specifier-ast)
+  (node* (:application)
+    (1 :function-name (make-function-name-ast 'list))
+    (* :argument
+       (list (node* (:literal :value ':name))
+             (ico:name-ast slot-specifier-ast)))
+    (* :argument
+       (if (null (ico:initform-ast slot-specifier-ast))
+           '()
+           (list (node* (:literal :value ':initform))
+                 (ico:initform-ast slot-specifier-ast)
+                 (node* (:literal :value ':initfunction))
+                 (node* (:lambda)
+                   (1 :lambda-list (node* (:ordinary-lambda-list)))
+                   (1 :form (ico:initform-ast slot-specifier-ast))))))
+    (* :argument
+       (if (null (ico:initarg-name-asts slot-specifier-ast))
+           '()
+           (list (node* (:literal :value ':initargs))
+                 (node* (:quote
+                         :object
+                         (mapcar #'unparse
+                                 (ico:initarg-name-asts slot-specifier-ast)))))))
+    (* :argument
+       (list (node* (:literal :value ':readers))
+             (node* (:quote
+                     :object
+                     (append (mapcar #'unparse
+                                     (ico:reader-asts slot-specifier-ast))
+                             (mapcar #'unparse
+                                     (ico:accessor-asts slot-specifier-ast)))))))
+    (* :argument
+       (list (node* (:literal :value ':writers))
+             (node* (:application)
+               (1 :function-name (make-function-name-ast 'list))
+               (* :argument
+                  (loop for writer-ast in (ico:writer-asts slot-specifier-ast)
+                        collect (node* (:quote :object (unparse writer-ast)))))
+               ;; We cant quote this argument because we must
+               ;; construct names such as (SETF FOO) using NODE*.
+               (* :argument
+                  (loop for accessor-ast in (ico:accessor-asts slot-specifier-ast)
+                        collect
+                        (node* (:application)
+                          (1 :function-name (make-function-name-ast 'list))
+                          (1 :argument (node* (:quote :object 'setf)))
+                          (1 :argument
+                             (node* (:quote :object (unparse accessor-ast))))))))))
+    (* :argument
+       (if (null (ico:documentation-ast slot-specifier-ast))
+           '()
+           (list (node* (:literal :value ':documentation))
+                 (ico:documentation-ast slot-specifier-ast))))))
 
-(defun canonicalize-slot-specifier-ast (ast)
-  `(:name ,(ico:name (ico:name-ast ast))
-          ,@(let ((initform-ast (ico:initform-ast ast)))
-              (if (null initform-ast)
-                  '()
-                  `(:initform ,initform-ast
-                    :initfunction (lambda () ,initform-ast))))
-          :initargs
-          ,@(mapcar #'ico:name (ico:initarg-asts ast))
-          :readers
-          ,@(mapcar #'ico:name (ico:reader-asts ast))
-          ,@(mapcar #'ico:name (ico:accessor-asts ast))
-          :writers
-          ,@(mapcar #'ico:name (ico:writer-asts ast))
-          ,@(mapcar (lambda (name) `(setf ,name))
-                    (ico:accessor-asts ast))
-          ,@(let ((documentation-ast (ico:documentation-ast ast)))
-              (if (null documentation-ast)
-                  '()
-                  `(:documentation
-                    ,(ico:documentation documentation-ast))))
-          ,@(let ((allocation (ico:allocation ast)))
-              (if (null allocation)
-                  '()
-                  `(:allocation ,allocation)))))
-
-(defun canonicalize-default-initarg-ast (ast)
-  (list (ico:name (ico:name-ast ast))
-        `',(ico:form (ico:initform-ast ast))
-        `(lambda () ,(ico:form (ico:initform-ast ast)))))
-
-(defmacro cmd:defclass
-    (&whole form
-     class-name
-     superclass-names
-     slot-specifiers
-     &rest class-options)
-  (declare (ignore class-name superclass-names)
-           (ignore slot-specifiers class-options))
-  (let* ((builder (make-instance 'bld:builder))
-         (syntax (ses:find-syntax 'defclass))
-         (ast (ses:parse builder syntax form))
-         (name (ico:name (ico:name-ast ast))))
-    `(setf (find-class ,name)
-           (make-instance 'standard-class
-             :name ,name
-             :direct-superclasses
-             (mapcar #'find-class ,superclass-names)
-             :direct-slots
-             (list ,@(mapcar #'canonicalize-slot-specifier-ast
-                             (ico:slot-specifier-asts ast)))
-             ,@(let ((default-initarg-asts (ico:default-initarg-asts ast)))
-                 (if (null default-initarg-asts)
-                     '()
-                     `(:direct-default-initargs
-                       (list ,@(mapcar #'canonicalize-default-initarg-ast
-                                       default-initarg-asts)))))))))
+(defmethod expand (client (ast ico:defclass-ast) environment)
+  (node* (:application)
+    (1 :function-name (make-function-name-ast 'ensure-class))
+    (1 :argument (ico:name-ast ast))
+    (* :argument
+       (list (node* (:literal :value ':direct-superclasses))
+             (node* (:quote :object
+                            (mapcar #'unparse
+                                    (ico:superclass-asts ast))))))
+    (* :argument
+       (list (node* (:literal :value ':direct-slots))
+             (canonicalize-slot-specifier-ast (ico:slot-specifier-asts ast))))
+    (* :argument
+       (if (null (ico:default-initarg-asts ast))
+           '()
+           (list (node* (:literal :value ':direct-default-initargs))
+                 (node* (:application)
+                   (1 :function-name (make-function-name-ast 'list))
+                   (* :argument
+                      (loop for default-initarg-ast in (ico:default-initargs ast)
+                            collect
+                            (node* (:application)
+                              (1 :function-name (make-function-name-ast 'list))
+                              (1 :argument (ico:name-ast default-initarg-ast))
+                              (1 :argument (ico:initform-ast default-initarg-ast))
+                              (1 :argument
+                                 (node* (:lambda)
+                                   (1 :lambda-list (node* (:ordinary-lambda-list)))
+                                   (1 :form
+                                      (ico:initform-ast default-initarg-ast)))))))))))
+    (* :argument
+       (if (null (ico:metaclass-asts ast))
+           '()
+           (list (node* (:literal :value ':metaclass-initargs))
+                 (node* (:quote :object (unparse (ico:metaclass-ast ast)))))))
+    (* :argument
+       (if (null (ico:documentation ast))
+           '()
+           (list (node* (:literal :value ':documentation))
+                 (ico:documentation-ast ast))))))
