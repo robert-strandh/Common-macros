8b89c69c73efbece232eccbd033ec1bcb29d7b6d
Robert Strandh <robert.strandh@gmail.com>
2023-07-16 08:18:16 +0200

    Define DECLAIM as an AST transformer.

--- Code/declaim.lisp
+++ Code/declaim.lisp
@@ -1,6 +1,14 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:declaim (&rest declaration-specifiers)
-  `(progn ,@(mapcar (lambda (declaration-specifier)
-                      `(proclaim ',declaration-specifier))
-                    declaration-specifiers)))
+(defmethod expand (client (ast ico:declaim-ast) environment)
+  (declare (ignore client environment))
+  (node* (:eval-when)
+    (* :situation
+       (make-eval-when-situation-asts
+        :compile-toplevel :load-toplevel :execute))
+    (* :form
+       (loop for declaration-specifier-ast in (ico:declaration-specifier-asts ast)
+             collect
+             (application
+              'proclaim
+              (node* (:qoute :object (unparse declaration-specifier-ast))))))))
