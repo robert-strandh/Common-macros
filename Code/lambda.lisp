2acb604929601a31b6f7860c4c62c9053e33230e
Robert Strandh <robert.strandh@gmail.com>
2023-09-08 17:43:55 +0200

    Pout a LAMBDA-EXPRESSION and not a LAMBDA form inside FUNCTION.

--- Code/lambda.lisp
+++ Code/lambda.lisp
@@ -3,4 +3,6 @@
 (defmethod expand (client (ast ico:lambda-ast) environment)
   (declare (ignore client environment))
   (node* (:function)
-    (1 :name ast)))
+    (1 :name (node* (:lambda-expression)
+               (1 :lambda-list (ico:lambda-list-ast ast))
+               (* :form (ico:form-asts ast))))))
