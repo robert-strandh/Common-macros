813da745d5188a56a4c92cea53a7c2fdf705bb1e
Robert Strandh <robert.strandh@gmail.com>
2023-09-15 14:32:37 +0200

    Rewrite EXPAND method for PUSH using AST-SETF-EXPANSION.

--- Code/push.lisp
+++ Code/push.lisp
@@ -1,14 +1,20 @@
 (cl:in-package #:common-macros)
 
 (defmethod expand (client (ast ico:push-ast) environment)
-  (multiple-value-bind
-        (binding-asts store-variable-asts store-ast read-ast)
-      (expand-place-ast client (ico:place-ast ast) environment)
-    (let ((item-var (gensym)))
-      (alet* ((b (make-variable-name-ast item-var) (ico:item-ast ast))
-              binding-asts
-              (b (first store-variable-asts)
-                 (application
-                  'cons
-                  (make-variable-name-ast item-var) read-ast)))
-        store-ast))))
+  (let* ((item-definition (make-instance 'ico:variable-definition-ast
+                            :name (gensym)))
+         (item-reference (make-variable-reference-ast item-definition)))
+    (multiple-value-bind
+          (temporary-asts form-asts store-variable-asts store-ast read-ast)
+        (ast-setf-expansion client (ico:place-ast ast) environment)
+      (make-instance 'ico:let-ast
+        :binding-asts
+        (cons (make-let-binding-ast item-definition (ico:item-ast ast))
+              (mapcar #'make-let-binding-ast temporary-asts form-asts))
+        :form-asts
+        (make-instance 'ico:let-ast
+          :binding-asts
+          (list (make-let-binding-ast
+                 (first store-variable-asts)
+                 (application 'cons item-reference read-ast)))
+          :form-asts (list store-ast))))))
