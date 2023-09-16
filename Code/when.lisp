8b120392161673f27ae67f2a141b059a53383b08
Robert Strandh <robert.strandh@gmail.com>
2023-09-16 15:47:19 +0200

    Define method on UNPARSE-EXPAND for WHEN-AST.

--- Code/when.lisp
+++ Code/when.lisp
@@ -5,3 +5,9 @@
   (aif (ico:test-ast ast)
        (aprogn (ico:form-asts ast))
        (make-unparsed-form-ast 'nil)))
+
+(defmethod unparse-expand (builder (ast ico:when-ast))
+  `(if ,(ses:unparse builder t (ico:test-ast ast))
+       (progn ,@(loop for ast in (ico:form-asts ast)
+                      collect (ses:unparse builder t ast)))
+       nil))
