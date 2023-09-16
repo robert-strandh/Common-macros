4c5811f382f8750a34743c7c12991d3f6b68ac83
Robert Strandh <robert.strandh@gmail.com>
2023-09-16 15:47:37 +0200

    Define method on UNPARSE-EXPAND for UNLESS-AST.

--- Code/unless.lisp
+++ Code/unless.lisp
@@ -5,3 +5,9 @@
   (aif (ico:test-ast ast)
        (make-unparsed-form-ast 'nil)
        (aprogn (ico:form-asts ast))))
+
+(defmethod unparse-expand (builder (ast ico:unless-ast))
+  `(if ,(ses:unparse builder t (ico:test-ast ast))
+       nil
+       (progn ,@(loop for ast in (ico:form-asts ast)
+                      collect (ses:unparse builder t ast)))))
