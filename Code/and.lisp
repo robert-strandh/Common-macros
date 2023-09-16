4d896f49e6e3c981bf4b4f0edbbf7405fa422047
Robert Strandh <robert.strandh@gmail.com>
2023-09-16 16:16:06 +0200

    Define method on UNPARSE-EXPAND for AND-AST.

--- Code/and.lisp
+++ Code/and.lisp
@@ -11,3 +11,17 @@
            (aif (first form-asts)
                 (node* (:and) (* :form (rest form-asts)))
                 (aliteral 'nil))))))
+
+(defmethod unparse-expand (builder (ast ico:and-ast))
+  (let ((form-asts (ico:form-asts ast)))
+    (labels ((unparse-form-asts (form-asts)
+               (let ((first-form (ses:unparse builder t (first form-asts)))
+                     (rest-form-asts (rest form-asts)))
+                 (if (null rest-form-asts)
+                     first-form
+                     `(if ,first-form
+                          ,(unparse-form-asts rest-form-asts)
+                          nil)))))
+      (if (null form-asts)
+          't
+          (unparse-form-asts form-asts)))))
