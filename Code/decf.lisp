c5d5a300ca9c351f6553339b63d17f60cc581205
Robert Strandh <robert.strandh@gmail.com>
2023-09-21 11:05:35 +0200

    Rewrite method on expand for DECF to use AST-SETF-EXPANSION.

--- Code/decf.lisp
+++ Code/decf.lisp
@@ -2,13 +2,18 @@
 
 (defmethod expand (client (ast ico:decf-ast) environment)
   (multiple-value-bind
-        (binding-asts store-variable-asts store-ast read-ast)
-      (expand-place-ast client (ico:place-ast ast) environment)
-    (let ((delta-ast
-            (if (null (ico:delta-ast ast))
-                (make-unparsed-form-ast '1)
-                (ico:delta-ast ast))))
-      (alet* (binding-asts
-              (b (first store-variable-asts)
-                 (application '- read-ast delta-ast)))
-        store-ast))))
+        (temporary-asts form-asts store-variable-asts store-ast read-ast)
+      (ast-setf-expansion client (ico:place-ast ast) environment)
+    (let ((delta-ast (if (null (ico:delta-ast ast))
+                         (make-instance 'ico:literal-ast :literal '1)
+                         (ico:delta-ast ast))))
+      (make-instance 'ico:let-ast
+        :binding-asts
+        (mapcar #'make-let-binding-ast temporary-asts form-asts)
+        :form-asts
+        (list (make-instance 'ico:let-ast
+                :binding-asts
+                (list (make-let-binding-ast
+                       (first store-variable-asts)
+                       (application '- read-ast delta-ast)))
+                :form-asts (list store-ast)))))))
