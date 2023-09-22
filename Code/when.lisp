18e73a9f063c1614d97f35c2799387c1262cec3c
Robert Strandh <robert.strandh@gmail.com>
2023-09-22 18:19:21 +0200

    Define method on COMPUTE-MACRO-FUNCTION for WHEN.

--- Code/when.lisp
+++ Code/when.lisp
@@ -11,3 +11,13 @@
        (progn ,@(loop for ast in (ico:form-asts ast)
                       collect (ses:unparse builder t ast)))
        nil))
+
+(defmethod compute-macro-function (builder (operator (eql 'when)))
+  (let ((form-variable (gensym))
+        (environment-variable (gensym)))
+    (compile
+     nil
+     `(lambda (,form-variable ,environment-variable)
+        (declare (ignorable ,environment-variable))
+        (unparse-expand
+         ,builder (ses:parse ,builder 'when ,form-variable))))))
