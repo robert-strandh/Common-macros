f89c3d940d7fd30a0a3ea7997259eacefc1d61c8
Robert Strandh <robert.strandh@gmail.com>
2023-09-23 10:19:52 +0200

    Define method on UNPARSE-EXPAND for OR.

--- Code/or.lisp
+++ Code/or.lisp
@@ -13,3 +13,20 @@
                (aif (make-variable-name-ast name)
                     (make-variable-name-ast name)
                     (node* (:or) (* :form (rest form-asts))))))))))
+
+(defmethod unparse-expand (builder (ast ico:or-ast))
+  (let ((form-asts (ico:form-asts ast)))
+    (if (null form-asts)
+        'nil
+        (labels ((unparse-form-asts (form-asts)
+                   (let ((first-form
+                           (ses:unparse builder t (first form-asts)))
+                         (rest-form-asts (rest form-asts))
+                         (temp (gensym)))
+                     (if (null rest-form-asts)
+                         first-form
+                         `(let ((,temp ,first-form))
+                            (if ,temp
+                                ,temp
+                                ,(unparse-form-asts rest-form-asts)))))))
+          (unparse-form-asts form-asts)))))
