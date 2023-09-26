e5e24caa225235f523633b45de68953fea7beb20
Robert Strandh <robert.strandh@gmail.com>
2023-09-26 16:20:49 +0200

    Define macros MULTIPLE-VALUE-BIND and MULTIPLE-VALUE-LIST.

--- Common-macro-definitions/multiple-value-bind.lisp
+++ Common-macro-definitions/multiple-value-bind.lisp
@@ -1,1 +1,16 @@
 (cl:in-package #:common-macro-definitions)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Macro MULTIPLE-VALUE-BIND.
+;;;
+;;; We define this macro pretty much exactly as the HyperSpec says in
+;;; the "Notes:" section on the page describing MULTIPLE-VALUE-BIND.  
+
+(defmacro multiple-value-bind (variables values-form &body body)
+  (let ((rest-variable (gensym)))
+    `(multiple-value-call
+         (lambda (&optional ,@variables &rest ,rest-variable)
+           (declare (ignore ,rest-variable))
+           ,@body)
+       ,values-form)))
--- Common-macro-definitions/multiple-value-list.lisp
+++ Common-macro-definitions/multiple-value-list.lisp
@@ -1,1 +1,4 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro multiple-value-list (form)
+  `(multiple-value-call #'list ,form))
