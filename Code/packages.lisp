855eb8091ba143fc09790342a5fa37425daec7c8
Robert Strandh <robert.strandh@gmail.com>
2023-09-26 14:28:38 +0200

    Remove definition of package COMMON-MACRO-DEFINITIONS.

--- Code/packages.lisp
+++ Code/packages.lisp
@@ -1,63 +1,5 @@
 (cl:in-package #:common-lisp-user)
 
-(defpackage #:common-macro-definitions
-  (:use)
-  (:export
-   #:when
-   #:unless
-   #:and
-   #:or
-   #:cond
-   #:case
-   #:ecase
-   #:ccase
-   #:incf
-   #:decf
-   #:push
-   #:pop
-   #:return
-   #:multiple-value-bind
-   #:multiple-value-list
-   #:multiple-value-setq
-   #:nth-value
-   #:shiftf
-   #:rotatef
-   #:defmethod
-   #:defvar
-   #:defpackage
-   #:defparameter
-   #:defun
-   #:defclass
-   #:define-condition
-   #:defgeneric
-   #:declaim
-   #:lambda
-   #:prog
-   #:prog*
-   #:prog1
-   #:prog2
-   #:pushnew
-   #:remf
-   #:ignore-errors
-   #:in-package
-   #:check-type
-   #:dolist
-   #:dotimes
-   #:do
-   #:do*
-   #:setf
-   #:psetf
-   #:psetq
-   #:restart-case
-   #:typecase
-   #:with-accessors
-   #:with-input-from-string
-   #:with-open-file
-   #:with-open-stream
-   #:with-output-to-string
-   #:with-simple-restart
-   #:with-slots))
-
 (defpackage #:common-macros
   (:use #:common-lisp)
   (:shadow #:get-setf-expansion)
@@ -65,8 +7,7 @@
    (#:ico #:iconoclast)
    (#:bld #:iconoclast-builder)
    (#:abp #:architecture.builder-protocol)
-   (#:ses #:s-expression-syntax)
-   (#:cmd #:common-macro-definitions))
+   (#:ses #:s-expression-syntax))
   (:export #:with-builder
            #:with-ast-origin
            #:destructure-lambda-list
