5eb0dfb9dac74f21d9a4cb1bc8657269adf01e99
Robert Strandh <robert.strandh@gmail.com>
2023-09-26 14:29:03 +0200

    Remove or comment out code for macros.

--- Code/ecase.lisp
+++ Code/ecase.lisp
@@ -1,34 +1,34 @@
 (cl:in-package #:common-macros)
 
-(defmacro cmd:ecase (&whole form keyform &rest clauses)
-  (declare (ignore keyform clauses))
-  (let* ((builder (make-instance 'bld:builder))
-         (syntax (ses:find-syntax 'ecase))
-         (ast (ses:parse builder syntax form))
-         (keyform-ast (ico:form-ast ast))
-         (keyform (ico:form keyform-ast))
-         (form-variable (gensym)))
-    (labels ((process-clauses (clause-asts)
-               (if (null clause-asts)
-                   `(error 'type-error
-                           :datum ,form-variable
-                           :expected-type
-                           '(member ,@(loop for clause-ast
-                                              in (ico:clause-asts ast)
-                                            for key-asts
-                                              = (ico:key-asts clause-ast)
-                                            append
-                                            (mapcar #'ico:key key-asts))))
-                   (let* ((clause-ast (first clause-asts))
-                          (form-asts (ico:form-asts clause-ast))
-                          (forms (mapcar #'ico:form form-asts)))
-                     (let* ((key-asts (ico:key-asts clause-ast))
-                            (keys (mapcar #'ico:key key-asts))
-                            (tests (mapcar (lambda (key)
-                                             `(eql ,form-variable ,key))
-                                           keys)))
-                       `(if (or ,@tests)
-                            (progn ,@forms)
-                            ,(process-clauses (rest clause-asts))))))))
-      `(let (,form-variable ,keyform)
-         ,(process-clauses (ico:clause-asts ast))))))
+;; (defmacro cmd:ecase (&whole form keyform &rest clauses)
+;;   (declare (ignore keyform clauses))
+;;   (let* ((builder (make-instance 'bld:builder))
+;;          (syntax (ses:find-syntax 'ecase))
+;;          (ast (ses:parse builder syntax form))
+;;          (keyform-ast (ico:form-ast ast))
+;;          (keyform (ico:form keyform-ast))
+;;          (form-variable (gensym)))
+;;     (labels ((process-clauses (clause-asts)
+;;                (if (null clause-asts)
+;;                    `(error 'type-error
+;;                            :datum ,form-variable
+;;                            :expected-type
+;;                            '(member ,@(loop for clause-ast
+;;                                               in (ico:clause-asts ast)
+;;                                             for key-asts
+;;                                               = (ico:key-asts clause-ast)
+;;                                             append
+;;                                             (mapcar #'ico:key key-asts))))
+;;                    (let* ((clause-ast (first clause-asts))
+;;                           (form-asts (ico:form-asts clause-ast))
+;;                           (forms (mapcar #'ico:form form-asts)))
+;;                      (let* ((key-asts (ico:key-asts clause-ast))
+;;                             (keys (mapcar #'ico:key key-asts))
+;;                             (tests (mapcar (lambda (key)
+;;                                              `(eql ,form-variable ,key))
+;;                                            keys)))
+;;                        `(if (or ,@tests)
+;;                             (progn ,@forms)
+;;                             ,(process-clauses (rest clause-asts))))))))
+;;       `(let (,form-variable ,keyform)
+;;          ,(process-clauses (ico:clause-asts ast))))))
--- Code/restart-case.lisp
+++ Code/restart-case.lisp
@@ -1,72 +1,72 @@
 (cl:in-package #:common-macros)
 
-(defun transform-restart-case-options (clause-ast)
-  (with-accessors ((interactive-ast ico:interactive-ast)
-                   (report-ast ico:report-ast)
-                   (test-ast ico:test-ast))
-      clause-ast
-    `(,@(if (null interactive-ast)
-            '()
-            `(:interactive
-              (function 
-               ,(if (typep interactive-ast 'ico:function-name-ast)
-                    (ico:name interactive-ast)
-                    (ico:form (ico:form-ast interactive-ast))))))
-      ,@(if (null report-ast)
-            '()
-            `(:report
-              (function 
-               ,(cond ((typep report-ast 'ico:unparsed-form-ast)
-                       `(lambda (stream)
-                          (write-string ,(ico:form report-ast))))
-                      ((typep report-ast 'ico:function-name-ast)
-                       (ico:name report-ast))
-                      (t
-                       (ico:form (ico:form-ast report-ast)))))))
-      ,@(if (null test-ast)
-            '()
-            `(:test
-              (function 
-               ,(if (typep test-ast 'ico:function-name-ast)
-                    (ico:name test-ast)
-                    (ico:form (ico:form-ast test-ast)))))))))
+;; (defun transform-restart-case-options (clause-ast)
+;;   (with-accessors ((interactive-ast ico:interactive-ast)
+;;                    (report-ast ico:report-ast)
+;;                    (test-ast ico:test-ast))
+;;       clause-ast
+;;     `(,@(if (null interactive-ast)
+;;             '()
+;;             `(:interactive
+;;               (function 
+;;                ,(if (typep interactive-ast 'ico:function-name-ast)
+;;                     (ico:name interactive-ast)
+;;                     (ico:form (ico:form-ast interactive-ast))))))
+;;       ,@(if (null report-ast)
+;;             '()
+;;             `(:report
+;;               (function 
+;;                ,(cond ((typep report-ast 'ico:unparsed-form-ast)
+;;                        `(lambda (stream)
+;;                           (write-string ,(ico:form report-ast))))
+;;                       ((typep report-ast 'ico:function-name-ast)
+;;                        (ico:name report-ast))
+;;                       (t
+;;                        (ico:form (ico:form-ast report-ast)))))))
+;;       ,@(if (null test-ast)
+;;             '()
+;;             `(:test
+;;               (function 
+;;                ,(if (typep test-ast 'ico:function-name-ast)
+;;                     (ico:name test-ast)
+;;                     (ico:form (ico:form-ast test-ast)))))))))
 
-;;; FIXME: handle restarts associated with a condition.
-(defmacro cmd:restart-case (&whole form restartable-form &rest clauses)
-  (declare (ignore restartable-form clauses))
-  (let* ((builder (make-instance 'bld:builder))
-         (syntax (ses:find-syntax 'restart-case))
-         (ast (ses:parse builder syntax form))
-         (clause-asts (ico:clause-asts ast)))
-    (let ((block-name (gensym))
-          (variable-name (gensym))
-          (tagbody-tags (loop for clause-ast in clause-asts
-                              collect (gensym))))
-      `(block ,block-name
-         (let ((,variable-name nil))
-           (tagbody
-              (restart-bind
-                  ,(loop for clause-ast in clause-asts
-                         for tagbody-tag in tagbody-tags
-                         collect
-                         `((,(ico:name (ico:name-ast clause-ast))
-                            (lambda (&rest temp)
-                              (setq ,variable-name temp)
-                              (go ,tagbody-tag)))
-                           ,@(transform-restart-case-options clause-ast)))
-                (return-from ,block-name ,(ico:form (ico:form-ast ast))))
-              ,@(loop for clause-ast in clause-asts
-                      for tagbody-tag in tagbody-tags
-                      collect tagbody-tag
-                      collect `(return-from ,block-name
-                                 (apply (lambda
-                                          ,(ico:unparse-lambda-list-ast
-                                            (ico:lambda-list-ast ast))
-                                          ;; FIXME: handle declarations
-                                          ,@(loop for form-ast
-                                                    in (ico:form-asts ast)
-                                                  collect
-                                                  (ico:form form-ast)))
-                                        ,variable-name)))))))))
+;; ;;; FIXME: handle restarts associated with a condition.
+;; (defmacro cmd:restart-case (&whole form restartable-form &rest clauses)
+;;   (declare (ignore restartable-form clauses))
+;;   (let* ((builder (make-instance 'bld:builder))
+;;          (syntax (ses:find-syntax 'restart-case))
+;;          (ast (ses:parse builder syntax form))
+;;          (clause-asts (ico:clause-asts ast)))
+;;     (let ((block-name (gensym))
+;;           (variable-name (gensym))
+;;           (tagbody-tags (loop for clause-ast in clause-asts
+;;                               collect (gensym))))
+;;       `(block ,block-name
+;;          (let ((,variable-name nil))
+;;            (tagbody
+;;               (restart-bind
+;;                   ,(loop for clause-ast in clause-asts
+;;                          for tagbody-tag in tagbody-tags
+;;                          collect
+;;                          `((,(ico:name (ico:name-ast clause-ast))
+;;                             (lambda (&rest temp)
+;;                               (setq ,variable-name temp)
+;;                               (go ,tagbody-tag)))
+;;                            ,@(transform-restart-case-options clause-ast)))
+;;                 (return-from ,block-name ,(ico:form (ico:form-ast ast))))
+;;               ,@(loop for clause-ast in clause-asts
+;;                       for tagbody-tag in tagbody-tags
+;;                       collect tagbody-tag
+;;                       collect `(return-from ,block-name
+;;                                  (apply (lambda
+;;                                           ,(ico:unparse-lambda-list-ast
+;;                                             (ico:lambda-list-ast ast))
+;;                                           ;; FIXME: handle declarations
+;;                                           ,@(loop for form-ast
+;;                                                     in (ico:form-asts ast)
+;;                                                   collect
+;;                                                   (ico:form form-ast)))
+;;                                         ,variable-name)))))))))
                                         
         
