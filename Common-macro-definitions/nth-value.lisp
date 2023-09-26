6365cb2fd967c6ad4483a7c5041d90a674a8e111
Robert Strandh <robert.strandh@gmail.com>
2023-09-26 16:21:14 +0200

    Define macros NTH-VALUE and RETURN.

--- Common-macro-definitions/nth-value.lisp
+++ Common-macro-definitions/nth-value.lisp
@@ -1,1 +1,4 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro nth-value (n form)
+  `(nth ,n (multiple-value-list ,form)))
--- Common-macro-definitions/return.lisp
+++ Common-macro-definitions/return.lisp
@@ -1,1 +1,4 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro return (&optional form)
+  `(return-from nil ,form))
