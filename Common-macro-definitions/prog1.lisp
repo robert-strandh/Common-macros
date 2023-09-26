6be0d4fd02db79e4297e036c73876953df7db043
Robert Strandh <robert.strandh@gmail.com>
2023-09-27 01:57:41 +0200

    Define macros PROG1 and PROG2.

--- Common-macro-definitions/prog1.lisp
+++ Common-macro-definitions/prog1.lisp
@@ -1,1 +1,7 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro prog1 (first-form &rest forms)
+  (let ((temp-var (gensym)))
+    `(let ((,temp-var ,first-form))
+       ,@forms
+       ,temp-var)))
--- Common-macro-definitions/prog2.lisp
+++ Common-macro-definitions/prog2.lisp
@@ -1,1 +1,9 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro prog2 (first-form second-form &rest forms)
+  (let ((temp-var (gensym)))
+    `(progn
+       ,first-form
+       (let ((,temp-var ,second-form))
+         ,@forms
+         ,temp-var))))
