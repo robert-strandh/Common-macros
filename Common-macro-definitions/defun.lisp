a99f2f61adc1f187fd64ed72d466c584815f99ae
Robert Strandh <robert.strandh@gmail.com>
2023-09-26 16:42:44 +0200

    Define macro DEFUN.

--- Common-macro-definitions/defun.lisp
+++ Common-macro-definitions/defun.lisp
@@ -1,1 +1,22 @@
 (cl:in-package #:common-macro-definitions)
+;;; FIXME: Do more syntax verification.
+
+(defgeneric defun-compile-time-action
+    (client name lambda-list environment))
+
+(defmacro defun (&environment environment name lambda-list &body body)
+  (multiple-value-bind (declarations documentation forms)
+      (separate-function-body body)
+    `(progn
+       (eval-when (:compile-toplevel)
+         ,(defun-compile-time-action name lambda-list environment))
+       (eval-when (:load-toplevel :execute)
+         (setf (fdefinition ',name)
+               (lambda ,lambda-list
+                 ,@declarations
+                 ,@(if (null documentation)
+                       '()
+                       (list documentation))
+                 (block ,(if (symbolp name) name (second name))
+                   ,@forms)))
+         ',name))))
