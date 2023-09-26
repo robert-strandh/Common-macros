0d0f71bfee7a0a6f25a429cafe3da329dc247b66
Robert Strandh <robert.strandh@gmail.com>
2023-09-26 16:31:07 +0200

    Define macros DEFVAR and DEFPARAMETER.

--- Common-macro-definitions/defparameter.lisp
+++ Common-macro-definitions/defparameter.lisp
@@ -1,1 +1,9 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro defparameter (name initial-value 
+                        &optional (documentation nil documentation-p))
+  `(progn (declaim (special ,name))
+          (setf (symbol-value ',name) ,initial-value)
+          ,(when documentation-p
+             `(setf (documentation ',name 'variable) ',documentation))
+          ',name))
--- Common-macro-definitions/defvar.lisp
+++ Common-macro-definitions/defvar.lisp
@@ -1,1 +1,12 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro defvar (name &optional
+                         (initial-value nil initial-value-p)
+                         (documentation nil documentation-p))
+  `(progn (declaim (special ,name))
+          ,(when initial-value-p
+             `(unless (boundp ',name)
+                (setf (symbol-value ',name) ,initial-value)))
+          ,(when documentation-p
+             `(setf (documentation ',name 'variable) ',documentation))
+          ',name))
