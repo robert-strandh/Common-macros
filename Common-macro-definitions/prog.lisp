f99fe93fea96f823004871cd14cef4b50bb13926
Robert Strandh <robert.strandh@gmail.com>
2023-09-27 01:56:18 +0200

    Define macros PROG and PROG*.

--- Common-macro-definitions/prog.lisp
+++ Common-macro-definitions/prog.lisp
@@ -1,1 +1,9 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro prog (bindings &body body)
+  (multiple-value-bind (declarations items)
+      (separate-ordinary-body body)
+    `(block nil
+       (let ,bindings
+         ,@declarations
+         (tagbody ,@items)))))
