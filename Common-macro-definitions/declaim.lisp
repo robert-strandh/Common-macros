9ab8e11425462c1ae52bc67ca8e43b10e92053ce
Robert Strandh <robert.strandh@gmail.com>
2023-09-27 01:49:24 +0200

    Define macro DECLAIM.

--- Common-macro-definitions/declaim.lisp
+++ Common-macro-definitions/declaim.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:common-macro-definitions)
+
+(defgeneric proclaim (declaration-specifier))
+
+(defmacro declaim (declaration-specifiers)
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     ,@(loop for declaration-specifier in declaration-specifiers
+             collect (proclaim declaration-specifier))))
