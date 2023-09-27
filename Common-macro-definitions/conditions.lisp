7b02cad8f7eba1604a02759ddc7e744efa02a7af
Robert Strandh <robert.strandh@gmail.com>
2023-09-27 06:45:56 +0200

    Define condition types for packages.

--- Common-macro-definitions/conditions.lisp
+++ Common-macro-definitions/conditions.lisp
@@ -94,3 +94,41 @@
                      ~s"
                      (type-error-datum condition)
                      (type-error-expected-type condition)))))
+
+(define-condition not-a-package-designator (type-error)
+  ()
+  (:report (lambda (condition stream)
+             (format stream
+                     "A package designator was required,~@
+                      but the following was given:~@
+                      ~s"
+                     (type-error-datum condition))))
+  (:default-initargs :expected-type '(or package character string symbol)))
+
+(define-condition nicknames-must-be-proper-list (type-error)
+  ()
+  (:default-initargs :expected-type 'list)
+  (:report (lambda (condition stream)
+             (format stream
+                     "The list of nicknames must be a proper list,~@
+                      but the following was found instead:~@
+                      ~s"
+                     (type-error-datum condition)))))
+
+(define-condition use-list-must-be-proper-list (type-error)
+  ()
+  (:default-initargs :expected-type 'list)
+  (:report (lambda (condition stream)
+             (format stream
+                     "The list of used packages must be a proper list,~@
+                      but the following was found instead:~@
+                      ~s"
+                     (type-error-datum condition)))))
+
+(define-condition symbol-conflict (package-error)
+  ((%conflicting-symbols
+    :initarg :conflicting-symbols
+    :reader conflicting-symbols)))
+
+(define-condition symbol-is-not-accessible (package-error)
+  ((%symbol :initarg :symbol :reader inaccessible-symbol)))
