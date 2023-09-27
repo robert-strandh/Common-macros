438ce8824146be6f086ee7eb8c0c855f9a39f207
Robert Strandh <robert.strandh@gmail.com>
2023-09-27 09:06:19 +0200

    Define macro WITH-SIMPLE-RESTART.

--- Common-macro-definitions/with-simple-restart.lisp
+++ Common-macro-definitions/with-simple-restart.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro with-simple-restart ((name format-control &rest args) &body forms)
+  (let ((stream (gensym "STREAM")))
+    `(restart-case ,(if (= 1 (length forms)) (car forms) `(progn ,@forms))
+       (,name ()
+         :report (lambda (,stream) (format ,stream ,format-control ,@args))
+         (values nil t)))))
