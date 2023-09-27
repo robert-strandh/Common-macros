847aa1527f231d11dc39c366ced55d357f1779bb
Robert Strandh <robert.strandh@gmail.com>
2023-09-27 07:05:25 +0200

    Define macros IN-PACKAGE, IGNORE-ERRORS, and CHECK-TYPE.

--- Common-macro-definitions/ignore-errors.lisp
+++ Common-macro-definitions/ignore-errors.lisp
@@ -1,1 +1,5 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro ignore-errors (&rest forms)
+  `(handler-case (progn ,@forms)
+     (error (condition) (values nil condition))))
--- Common-macro-definitions/in-package.lisp
+++ Common-macro-definitions/in-package.lisp
@@ -1,1 +1,5 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro in-package (string-designator)
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (setq *package* (find-package ',string-designator))))
