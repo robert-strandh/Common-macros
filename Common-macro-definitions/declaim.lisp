85b5edefebb7fb0a397a6f547c2ef5d89389f560
Robert Strandh <robert.strandh@gmail.com>
2023-10-16 05:16:35 +0200

    Add CLIENT parameter and argument to PROCLAIM.

--- Common-macro-definitions/declaim.lisp
+++ Common-macro-definitions/declaim.lisp
@@ -1,8 +1,8 @@
 (cl:in-package #:common-macro-definitions)
 
-(defgeneric proclaim (declaration-specifier environment))
+(defgeneric proclaim (client declaration-specifier environment))
 
 (defmacro declaim (&environment environment declaration-specifiers)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
      ,@(loop for declaration-specifier in declaration-specifiers
-             collect (proclaim declaration-specifier environment))))
+             collect (proclaim *client* declaration-specifier environment))))
