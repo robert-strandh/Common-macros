a284c31e2a12ecbd96320a465a19dbf22fcd59b6
Alex Wood <aeshtaer@gmail.com>
2023-10-18 13:14:23 -0400

    Allow compiler macros to work with FUNCALL forms

--- Common-macro-definitions/define-compiler-macro.lisp
+++ Common-macro-definitions/define-compiler-macro.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:common-macro-definitions)
 
 (defmacro define-compiler-macro (name lambda-list &body body)
-  (let ((expansion (ecc:parse-macro name lambda-list body)))
+  (let ((expansion (ecc:parse-compiler-macro name lambda-list body)))
     `(eval-when (:compile-toplevel :load-toplevel :execute)
        (setf (compiler-macro-function ',name) ,expansion))))
