2df36f380ebd7f2c02cd1560b94cc4d33233d58b
Alex Wood <aeshtaer@gmail.com>
2023-10-18 13:11:44 -0400

    Define m-v-setq

--- Common-macro-definitions/multiple-value-setq.lisp
+++ Common-macro-definitions/multiple-value-setq.lisp
@@ -1,1 +1,9 @@
 (cl:in-package #:common-macro-definitions)
+
+(defmacro multiple-value-setq (vars value-form)
+  (if (null vars)
+      ;; skip SETF if unnecessary.
+      `(values ,value-form)
+      ;; As defined in CLHS.
+      ;; FIXME: Validate that vars are symbols.
+      `(values (setf (values ,@vars) ,value-form))))
