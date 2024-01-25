67b9bf69a6c5729641bf3bcf23e69c67a9ebdc3d
Robert Strandh <robert.strandh@gmail.com>
2024-01-25 10:49:06 +0100

    Replace defective REMF macro.

--- Common-macro-definitions/remf.lisp
+++ Common-macro-definitions/remf.lisp
@@ -1,37 +1,39 @@
 (cl:in-package #:common-macro-definitions)
 
-(defmacro remf (place indicator &environment environment)
+(defmacro remf (&environment environment place indicator)
   (multiple-value-bind (vars vals store-vars writer-form reader-form)
       (get-setf-expansion place environment)
-    (let ((indicator-value-variable (gensym))
-          (store-var (car store-vars)))
-      `(macrolet ((maybe-error (datum predicate expected-type offending-list)
-                    `(unless (,predicate ,datum)
-                       (error 'must-be-property-list
-                              :datum ,datum
-                              :expected-type ',expected-type
-                              :offending-list ,offending-list))))
-         (block nil
-           (let ,(mapcar #'list vars vals)
-             (let* ((,store-var ,reader-form)
-                    (,indicator-value-variable ,indicator))
-               (when (null ,store-var)
-                 (return nil))
-               (maybe-error ,store-var consp list ,store-var)
-               (maybe-error (cdr ,store-var) consp cons ,store-var)
-               (when (eq ,indicator-value-variable (car ,store-var))
-                 (setq ,store-var (cddr ,store-var))
-                 ,writer-form
-                 (return t))
-               (loop for rest on (cdr ,store-var) by #'cddr
-                     ;; We know that REST is a CONS cell.
-                     do (when (null (cdr rest))
-                          ;; There are no more pairs to test, so we are
-                          ;; done.
-                          (return nil))
-                        (maybe-error (cdr rest) atom list ,store-var)
-                        (maybe-error (cddr rest) atom cons ,store-var)
-                        (when (eq ,indicator-value-variable (cadr rest))
-                          ;; We found a match.
-                          (setf (cdr rest) (cdddr rest))
-                          (return t))))))))))
+    (let ((indicator-var (gensym)))
+      `(let* (,@(if (null vars)
+                    `()
+                    `((,(first vars) ,(first vals))))
+              (,(first store-vars) ,reader-form)
+              (,indicator-var ,indicator))
+         (cond ((atom ,(first store-vars))
+                (if (null ,(first store-vars))
+                    nil
+                    (error 'must-be-plist :datum ',store-vars
+                                          :expected-type 'list)))
+               ((eq (car ,(first store-vars)) ,indicator-var)
+                (cond ((atom (cdr ,(first store-vars)))
+                       (error 'must-be-plist :datum ',store-vars
+                                             :expected-type 'list))
+                      (t
+                       (setq ,(first store-vars)
+                             (cddr ,(first store-vars)))
+                       ,writer-form
+                       t)))
+               (t
+                (loop for rest = (cdr ,(first store-vars)) then (cddr rest)
+                      until (atom (cdr rest))
+                      do (cond ((atom (cddr rest))
+                                (error 'must-be-plist :datum ',store-vars
+                                                      :expected-type 'list))
+                               ((eq (cadr rest) ,indicator-var)
+                                (setf (cdr rest) (cdddr rest))
+                                (return t))
+                               (t
+                                nil))
+                      finally (unless (null (cdr rest))
+                                (error 'must-be-plist :datum ',store-vars
+                                                      :expected-type 'list)))))))))
