33ac60f212e64b9acc078be121e30c7544dd944c
Robert Strandh <robert.strandh@gmail.com>
2024-03-08 15:42:14 +0100

    Add missing definitions.

--- Common-macro-definitions/check-type.lisp
+++ Common-macro-definitions/check-type.lisp
@@ -1,5 +1,25 @@
 (cl:in-package #:common-macro-definitions)
 
+(defun store-value-read-evaluated-form ()
+  (format *query-io* "~&;; Type a form to be evaluated:~%")
+  (list (eval (read *query-io*))))
+
+(defmacro with-store-value-restart ((temp-var place tag) &body forms)
+  (let ((report-var (gensym "STORE-VALUE-REPORT"))
+        (new-value-var (gensym "NEW-VALUE"))
+        (form-or-forms (if (= 1 (length forms))
+                           (first forms)
+                           `(progn ,@forms))))
+    `(flet ((,report-var (stream)
+              (format stream "Supply a new value of ~S." ',place)))
+       (restart-case ,form-or-forms
+         (store-value (,new-value-var)
+           :report ,report-var
+           :interactive store-value-read-evaluated-form
+           (setf ,temp-var ,new-value-var
+                 ,place ,new-value-var)
+           (go ,tag))))))
+
 (defun check-type-error (place value type type-string)
   (error
    'simple-type-error
