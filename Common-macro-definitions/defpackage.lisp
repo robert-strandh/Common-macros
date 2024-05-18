164ca5039cb743ccb4c3e6f547f91c8d7fed75b3
Robert Strandh <robert.strandh@gmail.com>
2024-05-18 15:19:19 +0200

    Define generic function ADD-LOCAL-NICKNAME.

--- Common-macro-definitions/defpackage.lisp
+++ Common-macro-definitions/defpackage.lisp
@@ -1,4 +1,6 @@
 (cl:in-package #:common-macro-definitions)
 
+(defgeneric add-local-nickname (client))
+
 (defmacro defpackage (name &rest options)
-  (expand-defpackage name options))
+  (expand-defpackage name options (add-local-nickname *client*)))
