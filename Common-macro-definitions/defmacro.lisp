b8ea7985a7684815d67f0c4c2a5a6d372f49f9a7
Robert Strandh <robert.strandh@gmail.com>
2024-12-17 16:37:28 +0100

    Use canonicalizing version of parse-macro.

--- Common-macro-definitions/defmacro.lisp
+++ Common-macro-definitions/defmacro.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:common-macro-definitions)
 
 (defmacro defmacro (name lambda-list &body body)
-  (let ((expansion (ecc:parse-macro name lambda-list body)))
+  (let ((expansion
+          (ecc:parse-macro-using-canonicalization name lambda-list body)))
     `(eval-when (:compile-toplevel :load-toplevel :execute)
        (setf (cl:macro-function ',name) ,expansion))))
